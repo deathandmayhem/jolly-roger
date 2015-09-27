32a694ab3d39d9939bf7e3327ca69970c1aef841
Evan Broder <evan@ebroder.net>
2015-09-27 11:23:37 -0400

    Add a flash warning for when the client gets disconnected

---
+++ client/views/layouts/main.js
@@ -0,0 +1,19 @@
+Template['layouts/main'].helpers({
+  statusIs(status) {
+    return Meteor.status().status === status;
+  },
+  reason() {
+    return Meteor.status().reason;
+  },
+  // Only valid if statusIs "waiting"
+  timeToRetry() {
+    return Math.floor((Meteor.status().retryTime - (new Date()).getTime()) / 1000);
+  }
+});
+
+Template['layouts/main'].events({
+  'click #jr-reconnect': function(event) {
+    Meteor.reconnect();
+    event.preventDefault();
+  }
+});
