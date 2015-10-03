50a27ae8b59a24725a73a82012b6d14cc015b4f7
Evan Broder <evan@ebroder.net>
2015-10-03 16:22:38 -0700

    Add a modal for creating a new hunt
    By reusing the same template for both the new hunt and list hunts
    route, and including a modal in the template, we can use a separate
    route for the new hunt page but have it work as a modal.
    
    I think this is a style we'll want for a bunch of editing constructs,
    though it wasn't totally straightforward. Notably, I couldn't use
    data-dismiss, which the Bootstrap docs recommend, since that would
    allow Bootstrap to get out of sync with Meteor state.

---
+++ client/views/hunts/index.js
@@ -0,0 +1,28 @@
+// Reuse the same options for both listing hunts and new hunt - latter
+// is a modal on top of the list
+const options = {
+  waitOn() {
+    return [
+      Meteor.subscribe('mongo.hunts')
+    ];
+  },
+  data() {
+    // TODO: sort?
+    return Models.Hunts.find();
+  }
+};
+
+Router.route('/hunts', _.extend({
+  name: 'hunts/index',
+}, options));
+Router.route('/hunts/new', _.extend({
+  name: 'hunts/new',
+  template: 'hunts/index',
+}, options));
+
+Template['hunts/index'].onRendered(function () {
+  this.autorun(() => {
+    const route = Router.current().route.getName();
+    $('#jr-hunt-new-modal').modal(route === 'hunts/new' ? 'show' : 'hide');
+  });
+});
--- client/views/index.js
+++ client/views/index.js
@@ -1,3 +1,3 @@
 Router.route('/', function () {
-  this.redirect('hunts/list');
+  this.redirect('hunts/index');
 });
