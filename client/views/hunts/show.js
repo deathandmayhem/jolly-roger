1f1d767faabc2b9e28815b4ed2c25f3cf4c6cd1d
Evan Broder <evan@ebroder.net>
2015-10-03 14:51:51 -0700

    Use _id instead if id in route variable
    This lets us be exceedingly clever with the pathFor template helper
    (we can pass an object, and Iron will automatically pull the _id field
    from it to fill in variables).

--- client/views/hunts/show.js
+++ client/views/hunts/show.js
@@ -1,12 +1,12 @@
-Router.route('/hunts/:id', {
+Router.route('/hunts/:_id', {
   name: 'hunts/show',
   waitOn() {
     return [
-      Meteor.subscribe('mongo.hunts', {_id: this.params.id}),
-      Meteor.subscribe('mongo.puzzles', {hunt: this.params.id})
+      Meteor.subscribe('mongo.hunts', {_id: this.params._id}),
+      Meteor.subscribe('mongo.puzzles', {hunt: this.params._id})
     ];
   },
   data() {
-    return Models.Hunts.findOne(this.params.id);
+    return Models.Hunts.findOne(this.params._id);
   }
 });
