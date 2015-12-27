f0d3f2570fea5782639cc523c4314d99b846f5f9
Evan Broder <evan@ebroder.net>
2015-12-27 17:49:42 +0100

    Add support for adding/removing rows
    Handsontable.Core.alter is how UI code triggers changes to the table
    topography, so intercept that and submit the changes to ShareJS
    instead.
    
    This doesn't implement add/remove columns. Given the current data
    representation, in order to do it we'd have to submit a separate
    operation to add the column to each row, and that won't compose
    correctly with other operations. I think we may have to define a
    custom OT to represent that (though it should be fairly
    straightforward).

--- client/components/Spreadsheet.jsx
+++ client/components/Spreadsheet.jsx
@@ -4,9 +4,8 @@
 // ShareJS want to be the ultimate owners of the data in question, and
 // we don't really want to spend our time shoveling data back and forth.
 //
-// Fortunately, in most cases, we can catch changes in Handsontable
-// before it applies them and cancel the update, submit it to ShareJS,
-// and let it bubble back to Handsontable from there.
+// To support this, we catch a bunch of changes that Handsontable
+// would otherwise make directly, and submit them ourselves to ShareJS
 
 Spreadsheet = React.createClass({
   componentDidMount() {
@@ -14,20 +13,28 @@ Spreadsheet = React.createClass({
     const doc = this.sharejs.get('docs', this.props.id || this.props.params.id);
     doc.subscribe();
     doc.whenReady(() => {
+      // Forcefully migrate from old format
+      if (doc.type && !doc.getSnapshot().data) {
+        doc.del();
+      }
+
       if (!doc.type) {
-        doc.create('json0', {rows: [
-          ['', '', '', '', ''],
-          ['', '', '', '', ''],
-          ['', '', '', '', ''],
-          ['', '', '', '', ''],
-        ],
+        doc.create('json0', {
+          rows: 4,
+          cols: 5,
+          data: [
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+          ],
         });
       }
 
-      ctx = doc.createContext();
-      ctx._onOp = () => $(this.refs.editor).handsontable('render');
+      const snap = doc.getSnapshot();
+      const ctx = doc.createContext();
 
-      $(this.refs.editor).handsontable({
+      this.hot = new Handsontable(this.refs.editor, {
         rowHeaders: true,
         manualRowResize: true,
         manualRowMove: true,
@@ -37,22 +44,62 @@ Spreadsheet = React.createClass({
         contextMenu: true,
         columnSorting: true,
         sortIndicator: true,
-        data: doc.getSnapshot().rows,
+        data: snap.data,
 
         beforeChange: (changes) => {
           changes.forEach(([r, c, last, cur]) => {
-            ctx.submitOp({p: ['rows', r, c], ld: last, li: cur});
+            ctx.submitOp({p: ['data', r, c], ld: last, li: cur});
           });
+
+          // Don't actually apply the modifications, since ShareJS will do it for us
           return false;
         },
       });
+
+      // This is super gross. We don't want Handsontable to apply the
+      // insert/delete column/row actions directly to the data source (we
+      // want ShareJS to do that), so we Monkeypatch alter so that we can
+      // send those operations to ShareJS
+      const _origAlter = this.hot.alter;
+      this.hot.alter = function(action, index, amount, source, keepEmptyRows) {
+        if (amount === undefined) {
+          amount = 1;
+        }
+
+        switch (action) {
+          case 'insert_row':
+            ctx.submitOp({p: ['rows'], na: amount});
+            while (amount-- > 0) {
+              ctx.submitOp({p: ['data', index], li: _.range(snap.cols).fill('')});
+            }
+
+            break;
+          case 'insert_col':
+            break;
+          case 'remove_row':
+            ctx.submitOp({p: ['rows'], na: -amount});
+            while (amount-- > 0) {
+              ctx.submitOp({p: ['data', index], ld: snap.data[index]});
+            }
+
+            break;
+          case 'remove_col':
+            break;
+          default:
+            throw new Error('There is no such action "' + action + '"');
+        }
+      };
+
+      // Trigger a re-render whenever an op comes in.
+      ctx._onOp = () => this.hot.render();
     });
   },
 
   componentWillUnmount() {
     if (this.sharejs) {
       this.sharejs.disconnect();
-      this.sharejs = null;
+      delete this.sharejs;
+      delete this.hot;
     }
   },
 
