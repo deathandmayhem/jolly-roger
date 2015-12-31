b8c4a36d4fec1ab6016213093933fac7886b5ad0
Evan Broder <evan@ebroder.net>
2015-12-31 18:43:30 +0100

    Configure mailgun

--- scripts/run_jolly_roger.sh
+++ scripts/run_jolly_roger.sh
@@ -7,4 +7,7 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/u
 if [ -z "${MONGO_URL+set}" ]; then
     export MONGO_URL="$(credstash get mongo)"
 fi
+if [ -z "${MAIL_URL+set}" ]; then
+    export MAIL_URL="$(credstash get mailgun)"
+fi
 exec bash $METEORD_DIR/run_app.sh
