c5a2ae2821ad9375a45911a62f099ec2dab37633
Evan Broder <evan@ebroder.net>
2016-01-08 20:30:35 +0100

    Add all emails, including those that are unverified
    Apparently we don't do a good job of verifying emails.

--- server/hunts.js
+++ server/hunts.js
@@ -14,14 +14,14 @@ Meteor.methods({
 
     Meteor.users.update(this.userId, {$addToSet: {hunts: huntId}});
     const user = Meteor.users.findOne(this.userId);
-    const email = _.chain(user.emails).
-      filter((email) => email.verified).
+    const emails = _.chain(user.emails).
       pluck('address').
-      first().
       value();
 
-    _.each(hunt.mailingLists, (list) => {
-      new Blanche.List(list).add(email);
+    _.each(emails, (email) => {
+      _.each(hunt.mailingLists, (list) => {
+        new Blanche.List(list).add(email);
+      });
     });
   },
 });
