79647204fe9eeeb913352563ed3b76ca54758e72
Drew Fisher <drew@sandstorm.io>
2016-01-09 13:10:47 -0800

    Fix puzzle creation hook (singular/plural typo)

--- server/puzzle.js
+++ server/puzzle.js
@@ -39,7 +39,7 @@ Meteor.methods({
     // The websocket listening for Slack messages should subscribe to that channel.
     // For documents, we should have a documents collection, with a puzzleId, type, and
     // type-specific data.
-    globalHooks.runPuzzleCreatedHook(puzzle);
+    globalHooks.runPuzzleCreatedHooks(puzzle);
 
     return puzzle;
   },
