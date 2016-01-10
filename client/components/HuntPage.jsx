82f001489351f28fc2e4c1f7a6ed711945a9db61
Drew Fisher <drew@sandstorm.io>
2016-01-10 00:52:13 -0800

    Display actual hunt title on HuntPage

--- client/components/HuntPage.jsx
+++ client/components/HuntPage.jsx
@@ -1,10 +1,20 @@
 const {Link} = ReactRouter;
 
 HuntPage = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    const huntHandle = Meteor.subscribe('mongo.hunts', {_id: this.props.params.huntId});
+    return {
+      hunt: Models.Hunts.findOne(this.props.params.huntId),
+    };
+  },
+
   render() {
+    const huntName = (this.data.hunt && this.data.hunt.name) || 'loading...';
     return (
       <div>
-        <h1>Hunt name</h1>
+        <h1>{huntName}</h1>
         <ul>
           <li><Link to={`/hunts/${this.props.params.huntId}/puzzles`}>Puzzles</Link></li>
           <li><Link to={`/hunts/${this.props.params.huntId}/announcements`}>Announcements</Link></li>
