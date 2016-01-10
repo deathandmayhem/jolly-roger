c3f686f6ebf97f0bb9ab76eef39d786d8e40d786
Evan Broder <evan@ebroder.net>
2016-01-11 00:08:49 +0100

    Prevent the app from crashing when Google OAuth creds are missing

--- server/gdrive.js
+++ server/gdrive.js
@@ -1,40 +1,49 @@
 // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
-const oauthConfig = ServiceConfiguration.configurations.findOne({service: 'google'});
-const oauthClient = new googleapis.auth.OAuth2(
-  oauthConfig.clientId,
-  oauthConfig.secret,
-  OAuth._redirectUri('google', oauthConfig));
+gdrive = null;
 
-// Override _postRequest so we can see if the access token got
-// refreshed
-oauthClient._postRequest = Meteor.bindEnvironment(function(err, result, response, callback) {
-  if (storedCredentials.accessToken !== this.credentials.access_token) {
-    Ansible.log('Storing refreshed access token for Google Drive');
-    Models.Settings.update({name: 'gdrive.credential'}, {
-      $set: {
-        'value.accessToken': this.credentials.access_token,
-        'value.refreshToken': this.credentials.refresh_token,
-        'value.expiresAt': this.credentials.expiry_date,
-      },
-    });
+Meteor.startup(() => {
+  const oauthConfig = ServiceConfiguration.configurations.findOne({service: 'google'});
+  if (oauthConfig === undefined) {
+    Ansible.log('Disabling gdrive integration because Google OAuth config is not loaded');
+    return;
   }
 
-  callback(err, result, response);
-}.bind(oauthClient));
+  const oauthClient = new googleapis.auth.OAuth2(
+    oauthConfig.clientId,
+    oauthConfig.secret,
+    OAuth._redirectUri('google', oauthConfig));
 
-let storedCredentials = {};
-const updateCredentials = function(token) {
-  storedCredentials = token.value;
+  // Override _postRequest so we can see if the access token got
+  // refreshed
+  oauthClient._postRequest = Meteor.bindEnvironment(function(err, result, response, callback) {
+    if (storedCredentials.accessToken !== this.credentials.access_token) {
+      Ansible.log('Storing refreshed access token for Google Drive');
+      Models.Settings.update({name: 'gdrive.credential'}, {
+        $set: {
+          'value.accessToken': this.credentials.access_token,
+          'value.refreshToken': this.credentials.refresh_token,
+          'value.expiresAt': this.credentials.expiry_date,
+        },
+      });
+    }
 
-  oauthClient.setCredentials({
-    access_token: token.value.accessToken,
-    refresh_token: token.value.refreshToken,
-    expiry_date: token.value.expiresAt,
-  });
-};
+    callback(err, result, response);
+  }.bind(oauthClient));
 
-const cursor = Models.Settings.find({name: 'gdrive.credential'});
-cursor.observe({added: updateCredentials, changed: updateCredentials});
+  let storedCredentials = {};
+  const updateCredentials = function(token) {
+    storedCredentials = token.value;
 
-gdrive = googleapis.drive({version: 'v3', auth: oauthClient});
+    oauthClient.setCredentials({
+      access_token: token.value.accessToken,
+      refresh_token: token.value.refreshToken,
+      expiry_date: token.value.expiresAt,
+    });
+  };
+
+  const cursor = Models.Settings.find({name: 'gdrive.credential'});
+  cursor.observe({added: updateCredentials, changed: updateCredentials});
+
+  gdrive = googleapis.drive({version: 'v3', auth: oauthClient});
+});
