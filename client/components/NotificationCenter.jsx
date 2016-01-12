5e36d0c9671d24c94fcf200b0891f666e7d62e7f
Evan Broder <evan@ebroder.net>
2016-01-12 16:02:48 +0100

    Show people without a Slack handle a Slack invite message
    This should make Slack onboarding a lot more straightforward.

--- client/components/NotificationCenter.jsx
+++ client/components/NotificationCenter.jsx
@@ -1,4 +1,5 @@
 const {PureRenderMixin} = React.addons;
+const {Link} = ReactRouter;
 
 const MessengerDismissButton = React.createClass({
   mixins: [PureRenderMixin],
@@ -18,39 +19,131 @@ const MessengerContent = React.createClass({
   },
 });
 
-const MessengerMessage = React.createClass({
+const MessengerSpinner = React.createClass({
   mixins: [PureRenderMixin],
+  render() {
+    return (
+      <div className="messenger-spinner">
+        <span className="messenger-spinner-side messenger-spinner-side-left">
+          <span className="messenger-spinner-fill"></span>
+        </span>
+        <span className="messenger-spinner-side messenger-spinner-side-right">
+          <span className="messenger-spinner-fill"></span>
+        </span>
+      </div>
+    );
+  },
+});
 
+const MessageMixin = {
   propTypes: {
     oldest: React.PropTypes.bool.isRequired,
     newest: React.PropTypes.bool.isRequired,
   },
 
-  render() {
-    classes = classnames(
+  messageClasses(type) {
+    return `messenger-message message alert ${type} message-${type} alert-${type}`;
+  },
+
+  slotClasses() {
+    return classnames(
       'messenger-message-slot messenger-shown',
       this.props.newest && 'messenger-last',
       this.props.oldest && 'messenger-first');
+  },
+};
+
+const MessengerMessage = React.createClass({
+  mixins: [PureRenderMixin, MessageMixin],
+
+  render() {
     return (
-      <li className={classes}>
-        <div className="messenger-message message alert info message-info alert-info">
+      <li className={this.slotClasses()}>
+        <div className={this.messageClasses('info')}>
           {this.props.children}
-          <div className="messenger-spinner">
-            <span className="messenger-spinner-side messenger-spinner-side-left">
-              <span className="messenger-spinner-fill"></span>
-            </span>
-            <span className="messenger-spinner-side messenger-spinner-side-right">
-              <span className="messenger-spinner-fill"></span>
-            </span>
-          </div>
+          <MessengerSpinner/>
         </div>
       </li>
     );
   },
 });
 
-const Announcement = React.createClass({
-  mixins: [PureRenderMixin],
+const SlackMessage = React.createClass({
+  mixins: [PureRenderMixin, MessageMixin],
+
+  getInitialState() {
+    return {status: 'idle', errorMessage: null};
+  },
+
+  sendInvite() {
+    this.setState({status: 'submitting'});
+    Meteor.call('slackInvite', (err) => {
+      if (err) {
+        this.setState({status: 'error', errorMessage: err.message});
+      } else {
+        this.setState({status: 'success'});
+      }
+    });
+  },
+
+  reset() {
+    this.setState({status: 'idle', errorMessage: null});
+  },
+
+  render() {
+    let msg;
+    let type;
+    switch (this.state.status) {
+      case 'idle':
+        msg = 'It looks like there\'s no Slack username in your profile. If you need an invite ' +
+              'to Slack, we can do that! Otherwise, adding it to your profile helps us get in ' +
+              'touch.';
+        type = 'info';
+        break;
+      case 'submitting':
+        msg = 'Sending an invite now...';
+        type = 'error';
+        break;
+      case 'success':
+        msg = 'Done! Check your email. This notification will stick around to remind you to ' +
+              'finish signing up.';
+        type = 'success';
+        break;
+      case 'error':
+        msg = `Uh-oh - something went wrong: ${this.state.errorMessage}`;
+        type = 'error';
+        break;
+    };
+
+    const actions = [];
+    if (this.state.status === 'idle') {
+      actions.push(<a onClick={this.sendInvite}>Send me an invite</a>);
+    }
+
+    actions.push(<Link to="/users/me">Edit my profile</Link>);
+
+    if (this.state.status === 'success' || this.state.status === 'error') {
+      actions.push(<a onClick={this.reset}>Ok</a>);
+    }
+
+    return (
+      <li className={this.slotClasses()}>
+        <div className={classnames(this.messageClasses(type), this.state.status === 'submitting' && 'messenger-retry-soon')}>
+          <MessengerContent>
+            {msg}
+          </MessengerContent>
+          <div className="messenger-actions">
+            {actions}
+          </div>
+          <MessengerSpinner/>
+        </div>
+      </li>
+    );
+  },
+});
+
+const AnnouncementMessage = React.createClass({
+  mixins: [PureRenderMixin, MessageMixin],
 
   propTypes: {
     id: React.PropTypes.string.isRequired,
@@ -64,13 +157,16 @@ const Announcement = React.createClass({
 
   render() {
     return (
-      <div>
-        <MessengerDismissButton onDismiss={this.onDismiss}/>
-        <MessengerContent>
-          <div dangerouslySetInnerHTML={{__html: marked(this.props.announcement.message, {sanitize: true})}}/>
-          <footer>- {this.props.createdBy.displayName}, {moment(this.props.announcement.createdAt).calendar()}</footer>
-        </MessengerContent>
-      </div>
+      <li className={this.slotClasses()}>
+        <div className={this.messageClasses('info')}>
+          <MessengerDismissButton onDismiss={this.onDismiss}/>
+          <MessengerContent>
+            <div dangerouslySetInnerHTML={{__html: marked(this.props.announcement.message, {sanitize: true})}}/>
+            <footer>- {this.props.createdBy.displayName}, {moment(this.props.announcement.createdAt).calendar()}</footer>
+          </MessengerContent>
+          <MessengerSpinner/>
+        </div>
+      </li>
     );
   },
 });
@@ -122,30 +218,36 @@ NotificationCenter = React.createClass({
       return <div/>;
     }
 
-    const announcements = _.map(this.data.announcements, (a, idx) => {
-      return (
-        <Announcement
-                key={a.pa._id}
-                id={a.pa._id}
-                announcement={a.announcement}
-                createdBy={a.createdBy}/>
-      );
+    // Build a list of uninstantiated messages with their props, then create them
+    const messages = [];
+    let i = 0;
+
+    const profile = Models.Profiles.findOne(Meteor.userId());
+    if (!profile || !profile.slackHandle) {
+      messages.push([SlackMessage, {key: 'slack'}]);
+    }
+
+    _.forEach(this.data.announcements, (a, idx) => {
+      messages.push([
+        AnnouncementMessage, {
+          key: a.pa._id,
+          id: a.pa._id,
+          announcement: a.announcement,
+          createdBy: a.createdBy,
+        },
+      ]);
     });
 
-    const messages = _.map(announcements, (m, idx) => {
-      return (
-        <MessengerMessage
-                key={m.props.id}
-                newest={idx === 0}
-                oldest={idx === this.data.announcements.length - 1}>
-          {m}
-        </MessengerMessage>
-      );
+    const instantiated = _.map(messages, ([kls, props], idx) => {
+      return React.createElement(kls, _.extend({
+        newest: idx === 0,
+        oldest: idx === messages.length - 1,
+      }, props));
     });
 
     return (
       <ul className="messenger messenger-fixed messenger-on-top messenger-on-right messenger-theme-flat">
-        {messages}
+        {instantiated}
       </ul>
     );
   },
---
+++ server/slack.js
@@ -0,0 +1,46 @@
+Meteor.methods({
+  slackInvite(userId) {
+    check(userId, Match.Optional(String));
+
+    if (!this.userId && this.connection) {
+      throw new Meteor.Error(403, 'Only logged in users can send slack invites');
+    }
+
+    if (!this.userId && !userId) {
+      throw new Meteor.Error(400, 'Must pass user id for server calls');
+    }
+
+    const user = Meteor.users.findOne(userId || this.userId);
+    if (!user) {
+      throw new Meteor.Error(404, 'Can not find user');
+    }
+
+    const email = user && user.emails && user.emails[0] && user.emails[0].address;
+    if (!email) {
+      throw new Meteor.Error(400, 'User does not have an email address on file');
+    }
+
+    const config = ServiceConfiguration.configurations.findOne({service: 'slack'});
+    if (!config) {
+      throw new Meteor.Error(500, 'Slack is not configured; unable to send invite');
+    }
+
+    this.unblock();
+
+    Ansible.log('Sending a Slack invite', {email, user: user._id, sender: this.userId});
+
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    const result = HTTP.post('https://slack.com/api/users.admin.invite', {
+      params: {
+        token: config.secret,
+        email,
+        set_active: true,
+      },
+    });
+
+    if (result.statusCode >= 400) {
+      Ansible.log('Error sending Slack invite', {content: result.content});
+      throw new Meteor.Error(500, `Something went wrong sending the invite`);
+    }
+  },
+});
