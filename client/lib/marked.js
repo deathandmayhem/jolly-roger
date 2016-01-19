21ac79398feb6011f441ad458886887ab6bba4ba
Evan Broder <evan@ebroder.net>
2016-01-19 08:51:26 -0500

    markdown links all get target=_blank

--- client/lib/marked.js
+++ client/lib/marked.js
@@ -1,2 +1,10 @@
 marked.InlineLexer.rules.gfm.em = /^\b_((?:__|[^_])+?)_\b/;
 marked.InlineLexer.rules.gfm.strong = /^\*\b((?:\*\*|[^\*])+?)\b\*/;
+
+const renderer = new class extends marked.Renderer {
+  link(href, title, link) {
+    return super(href, title, link).replace(/^<a /, '<a target="_blank" ');
+  }
+}();
+
+marked.setOptions({renderer});
