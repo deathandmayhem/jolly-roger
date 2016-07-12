e12aa7c49b73e0985635f0706e8546ab7387cef0
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-12 00:46:35 -0700

    Drop client-deps package (we've moved everything to npm/ES6 imports)

--- packages/client-deps/app.browserify.js
+++
@@ -1,9 +0,0 @@
-ReactBootstrap = require('react-bootstrap');
-ReactRouter = require('react-router');
-ReactRouter.history = require('history');
-ReactRouterBootstrap = require('react-router-bootstrap');
-DocumentTitle = require('react-document-title');
-
-classnames = require('classnames');
-
-marked = require('marked');
