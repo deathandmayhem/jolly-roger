9ef389544ba72a9f6d076b3ad525d58ab75c419f
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-14 12:38:25 -0700

    ESLint server/hunts.js

--- server/hunts.js
+++ server/hunts.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
 import Ansible from '/imports/ansible.js';
 import { List } from '/imports/server/blanche.js';
 import { huntFixtures } from '/imports/fixtures.js';
@@ -13,13 +14,13 @@ Meteor.methods({
       throw new Meteor.Error(401, 'You are not logged in');
     }
 
-    hunt = huntFixtures[huntId] || Models.Hunts.findOne(huntId);
+    const hunt = huntFixtures[huntId] || Models.Hunts.findOne(huntId);
     if (!hunt) {
       throw new Meteor.Error(404, 'Unknown hunt');
     }
 
-    Ansible.log('User joining hunt', {user: this.userId, hunt: huntId});
-    Meteor.users.update(this.userId, {$addToSet: {hunts: huntId}});
+    Ansible.log('User joining hunt', { user: this.userId, hunt: huntId });
+    Meteor.users.update(this.userId, { $addToSet: { hunts: huntId } });
     const user = Meteor.users.findOne(this.userId);
     const emails = _.chain(user.emails).
       pluck('address').
@@ -29,7 +30,7 @@ Meteor.methods({
       const list = new List(listName);
       _.each(emails, (email) => {
         if (!list.add(email)) {
-          Ansible.log('Unable to add user to list', {email, list: listName});
+          Ansible.log('Unable to add user to list', { email, list: listName });
         }
       });
     });
