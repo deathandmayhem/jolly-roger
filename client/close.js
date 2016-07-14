e5c15eae6f62db3e15284ec6c611d118f8e1517e
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-14 13:07:47 -0700

    ESLint client/close.js

--- client/close.js
+++ client/close.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+
+// eslint-disable-next-line consistent-return
 window.onbeforeunload = () => {
   if (Meteor.connection._anyMethodsAreOutstanding()) {
     return 'There are requests to the server that are still pending. If you ' +
