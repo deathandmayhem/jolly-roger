1200cb91555af88a0af67776a50417f4fab48ff4
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-14 13:29:29 -0700

    ESLint imports/server/blanche.js

--- imports/server/blanche.js
+++ imports/server/blanche.js
@@ -1,18 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
 import child from 'child_process';
 import Ansible from '/imports/ansible.js';
 
 const execFile = Meteor.wrapAsync(child.execFile);
 
-const blanche = (args, cb) => {
+const blanche = (args) => {
   try {
-    return execFile('blanche', args, {stdio: ['ignore', 'pipe', process.stderr]});
+    return execFile('blanche', args, { stdio: ['ignore', 'pipe', process.stderr] });
   } catch (e) {
     if (e.code !== 'ENOENT') {
       throw e;
     }
 
-    Ansible.warn('Would run blanche, but it\'s not available', {args: args});
+    Ansible.warn('Would run blanche, but it\'s not available', { args });
     return '';
   }
 };
@@ -24,12 +25,13 @@ class List {
 
   members() {
     const out = blanche([this.name]);
-    return _.compact(_.map(out.trim().split('\n'), (member) => {
+    return _.compact(_.map(out.trim().split('\n'), (line) => {
       // Technically some of these are probably type STRING, but the
       // distinction isn't important here
       let type = 'USER';
-      if (member.indexOf(':') !== -1) {
-        [type, member] = member.split(':');
+      let member = line;
+      if (line.indexOf(':') !== -1) {
+        [type, member] = line.split(':');
       }
 
       if (member.indexOf('@') === -1) {
@@ -37,9 +39,11 @@ class List {
       }
 
       switch (type) {
-      case 'USER':
-      case 'LIST':
-        return member;
+        case 'USER':
+        case 'LIST':
+          return member;
+        default:
+          return undefined;
       }
     }));
   }
