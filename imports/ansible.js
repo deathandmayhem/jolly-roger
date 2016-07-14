2c4908a473b6f31b7fb4168fbee3c4ddf50e93ab
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-14 13:30:26 -0700

    ESLint imports/ansible.js

--- imports/ansible.js
+++ imports/ansible.js
@@ -4,14 +4,14 @@ const Ansible = {};
 
 const logLevels = new Set(['log', 'info', 'error', 'warn']);
 logLevels.forEach(l => {
-  Ansible[l] = function(line, obj) {
-    args = [line];
+  Ansible[l] = function (line, obj) {
+    const args = [line];
     if (obj) {
       args.push(obj);
     }
 
     if (Meteor.isClient) {
-      console[l](...args);
+      console[l](...args); // eslint-disable-line no-console
     }
 
     Meteor.call('ansible', l, ...args);
