172f7446b673fac66ed6f8baaaa8942a02ec5f28
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-14 14:27:34 -0700

    ESLint imports/model-helpers.js

--- imports/model-helpers.js
+++ imports/model-helpers.js
@@ -1,27 +1,31 @@
-const answerify = function(answer) {
+import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
+
+
+const answerify = function (answer) {
   return answer.
     replace(/[^A-Za-z0-9]/g, '').
     toUpperCase();
 };
 
-const huntsMatchingCurrentUser = function(q) {
+const huntsMatchingCurrentUser = function (origQuery) {
   // Adds a filter to the query to only show results from hunts that the user is a member of.
   // Assumes the collection being published has a field named `hunt` of type String containing the
   // _id of a document from the Hunts collection.
   // The caller binds `this` to be the one provided to the function of Meteor.publish(), so
   // this.userId is available.
-  u = Meteor.users.findOne(this.userId);
-  q = _.clone(q);
+  const u = Meteor.users.findOne(this.userId);
+  let q = _.clone(origQuery);
 
   if (_.has(q, 'hunt')) {
     if (_.isObject(q.hunt) &&
-        _.some(q.hunt, (_, k) => k.startsWith('$'))) {
+        _.some(q.hunt, (unused, k) => k.startsWith('$'))) {
       q.hunt.$in = _.intersection(..._.compact([q.hunt.$in, u.hunts]));
     } else {
-      q.hunt = {$in: _.intersection([q.hunt], u.hunts)};
+      q.hunt = { $in: _.intersection([q.hunt], u.hunts) };
     }
   } else {
-    q = _.extend(q, {hunt: {$in: u.hunts}});
+    q = _.extend(q, { hunt: { $in: u.hunts } });
   }
 
   return q;
