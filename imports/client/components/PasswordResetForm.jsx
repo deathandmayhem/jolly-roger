4509038353a63ca278d69f1a9135a79b11847cc7
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-19 01:52:56 -0700

    ESLint PasswordResetForm

--- imports/client/components/PasswordResetForm.jsx
+++ imports/client/components/PasswordResetForm.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { AccountForm } from '/imports/client/components/AccountForm.jsx';
 
 const PasswordResetForm = React.createClass({
+  propTypes: {
+    params: React.PropTypes.shape({
+      token: React.PropTypes.string.isRequired,
+    }).isRequired,
+  },
+
   render() {
     return <AccountForm format="resetPwd" token={this.props.params.token} />;
   },
