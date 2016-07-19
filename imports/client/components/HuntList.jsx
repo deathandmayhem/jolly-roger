f0a2083bb5a74c371433768731ede4c098846aa6
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-19 01:34:38 -0700

    ESLint HuntList

--- imports/client/components/HuntList.jsx
+++ imports/client/components/HuntList.jsx
@@ -1,11 +1,14 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Link } from 'react-router';
 import BS from 'react-bootstrap';
-import Ansible from '/imports/ansible.js'
-import { huntFixtures } from '/imports/fixtures.js';
+import Ansible from '/imports/ansible.js';
+// import { huntFixtures } from '/imports/fixtures.js';
 import { JRPropTypes } from '/imports/client/JRPropTypes.js';
 import { ModalForm } from '/imports/client/components/ModalForm.jsx';
-// TODO: ReactMeteorData
+import { ReactMeteorData } from 'meteor/react-meteor-data';
+
+/* eslint-disable max-len */
 
 const HuntFormModal = React.createClass({
   propTypes: {
@@ -17,59 +20,46 @@ const HuntFormModal = React.createClass({
     const idPrefix = this.props.hunt ? `jr-hunt-${this.props.hunt.id}-modal-` : 'jr-hunt-new-modal-';
     return (
       <ModalForm
-          ref="form"
-          title={this.props.hunt ? 'Edit Hunt' : 'New Hunt'}
-          onSubmit={this.props.onSubmit}>
+        ref="form"
+        title={this.props.hunt ? 'Edit Hunt' : 'New Hunt'}
+        onSubmit={this.props.onSubmit}
+      >
         <BS.Input
-            id={`${idPrefix}name`}
-            ref="input:name"
-            type="text"
-            label="Name"
-            labelClassName="col-xs-3"
-            wrapperClassName="col-xs-9"
-            defaultValue={this.props.hunt && this.props.hunt.name}
-            autoFocus="true"/>
+          id={`${idPrefix}name`}
+          ref="input:name"
+          type="text"
+          label="Name"
+          labelClassName="col-xs-3"
+          wrapperClassName="col-xs-9"
+          defaultValue={this.props.hunt && this.props.hunt.name}
+          autoFocus="true"
+        />
         <BS.Input
-            id={`${idPrefix}mailing-lists`}
-            ref="input:mailingLists"
-            type="text"
-            label="Mailing lists"
-            help="Users joining this hunt will be automatically added to all of these (comma-separated) lists"
-            labelClassName="col-xs-3"
-            wrapperClassName="col-xs-9"
-            defaultValue={this.props.hunt && this.props.hunt.mailingLists && this.props.hunt.mailingLists.join(', ')}/>
+          id={`${idPrefix}mailing-lists`}
+          ref="input:mailingLists"
+          type="text"
+          label="Mailing lists"
+          help="Users joining this hunt will be automatically added to all of these (comma-separated) lists"
+          labelClassName="col-xs-3"
+          wrapperClassName="col-xs-9"
+          defaultValue={this.props.hunt && this.props.hunt.mailingLists && this.props.hunt.mailingLists.join(', ')}
+        />
       </ModalForm>
     );
   },
 });
 
 const Hunt = React.createClass({
-  mixins: [ReactMeteorData],
-
-  getMeteorData() {
-    return {
-      isOperator: Roles.userHasRole(Meteor.userId(), 'admin'),
-      canUpdate: Roles.userHasPermission(Meteor.userId(), 'mongo.hunts.update'),
-      canRemove: Roles.userHasPermission(Meteor.userId(), 'mongo.hunts.remove'),
-    };
-  },
-
   propTypes: {
     hunt: React.PropTypes.instanceOf(Transforms.Hunt).isRequired,
   },
 
-  showEditModal() {
-    this.refs.editModal.refs.form.show();
-  },
-
-  showDeleteModal() {
-    this.refs.deleteModal.show();
-  },
+  mixins: [ReactMeteorData],
 
   onEdit(callback) {
-    Ansible.log('Updating hunt settings', {hunt: this.props.hunt._id, user: Meteor.userId()});
+    Ansible.log('Updating hunt settings', { hunt: this.props.hunt._id, user: Meteor.userId() });
     Models.Hunts.update(
-      {_id: this.props.hunt._id},
+      { _id: this.props.hunt._id },
       {
         $set: {
           name: this.refs.editModal.refs['input:name'].getValue(),
@@ -84,24 +74,44 @@ const Hunt = React.createClass({
     this.props.hunt.destroy(callback);
   },
 
+  getMeteorData() {
+    return {
+      isOperator: Roles.userHasRole(Meteor.userId(), 'admin'),
+      canUpdate: Roles.userHasPermission(Meteor.userId(), 'mongo.hunts.update'),
+      canRemove: Roles.userHasPermission(Meteor.userId(), 'mongo.hunts.remove'),
+    };
+  },
+
+  showEditModal() {
+    this.refs.editModal.refs.form.show();
+  },
+
+  showDeleteModal() {
+    this.refs.deleteModal.show();
+  },
+
   editButton() {
     if (this.data.canUpdate) {
       return (
         <BS.Button onClick={this.showEditModal} bsStyle="default" title="Edit hunt...">
-          <BS.Glyphicon glyph="edit"/>
+          <BS.Glyphicon glyph="edit" />
         </BS.Button>
       );
     }
+
+    return undefined;
   },
 
   deleteButton() {
     if (this.data.canRemove) {
       return (
         <BS.Button onClick={this.showDeleteModal} bsStyle="danger" title="Delete hunt...">
-          <BS.Glyphicon glyph="remove"/>
+          <BS.Glyphicon glyph="remove" />
         </BS.Button>
       );
     }
+
+    return undefined;
   },
 
   render() {
@@ -109,15 +119,17 @@ const Hunt = React.createClass({
     return (
       <li>
         <HuntFormModal
-            ref="editModal"
-            hunt={this.props.hunt}
-            onSubmit={this.onEdit}/>
+          ref="editModal"
+          hunt={this.props.hunt}
+          onSubmit={this.onEdit}
+        />
         <ModalForm
-            ref="deleteModal"
-            title="Delete Hunt"
-            submitLabel="Delete"
-            submitStyle="danger"
-            onSubmit={this.onDelete}>
+          ref="deleteModal"
+          title="Delete Hunt"
+          submitLabel="Delete"
+          submitStyle="danger"
+          onSubmit={this.onDelete}
+        >
           Are you sure you want to delete "{this.props.hunt.name}"?
           This will additionally delete all puzzles and associated
           state.
@@ -134,6 +146,7 @@ const Hunt = React.createClass({
   },
 });
 
+/*
 const MockHunt = React.createClass({
   render() {
     return (
@@ -143,12 +156,23 @@ const MockHunt = React.createClass({
     );
   },
 });
+*/
 
 const HuntList = React.createClass({
-  mixins: [ReactMeteorData],
-
   contextTypes: {
     subs: JRPropTypes.subs,
+  },
+
+  mixins: [ReactMeteorData],
+
+  onAdd(callback) {
+    const name = this.refs.addModal.refs['input:name'].getValue();
+
+    Ansible.log('Creating a new hunt', { name, user: Meteor.userId() });
+    Models.Hunts.insert({
+      name,
+      mailingLists: this.refs.addModal.refs['input:mailingLists'].getValue().split(/[, ]+/),
+    }, callback);
   },
 
   getMeteorData() {
@@ -163,42 +187,35 @@ const HuntList = React.createClass({
     this.refs.addModal.refs.form.show();
   },
 
-  onAdd(callback) {
-    const name = this.refs.addModal.refs['input:name'].getValue();
-
-    Ansible.log('Creating a new hunt', {name, user: Meteor.userId()});
-    Models.Hunts.insert({
-      name,
-      mailingLists: this.refs.addModal.refs['input:mailingLists'].getValue().split(/[, ]+/),
-    }, callback);
-  },
-
   addButton() {
     if (this.data.canAdd) {
       return (
         <BS.Button onClick={this.showAddModal} bsStyle="success" bsSize="xs" title="Add new hunt...">
-          <BS.Glyphicon glyph="plus"/>
+          <BS.Glyphicon glyph="plus" />
         </BS.Button>
       );
     }
+
+    return undefined;
   },
 
   render() {
     const hunts = this.data.hunts.map((hunt) => {
-      return <Hunt key={hunt._id} hunt={hunt}/>;
+      return <Hunt key={hunt._id} hunt={hunt} />;
     });
 
-    // Insert mock data from 2015 hunt.
-    //_.each(huntFixtures, (mockData, id) => {
-    //  hunts.push(<MockHunt key={id} hunt={mockData}/>);
-    //});
+    //  Insert mock data from 2015 hunt.
+    // _.each(huntFixtures, (mockData, id) => {
+    //   hunts.push(<MockHunt key={id} hunt={mockData}/>);
+    // });
 
     return (
       <div id="jr-hunts">
         <h1>Hunts</h1>
         <HuntFormModal
-            ref="addModal"
-            onSubmit={this.onAdd}/>
+          ref="addModal"
+          onSubmit={this.onAdd}
+        />
         {this.addButton()}
         <ul>
           {hunts}
