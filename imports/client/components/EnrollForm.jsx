97b3a56d6e5384a67b1fc5ebb4390d78a7a42f47
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-19 01:21:43 -0700

    ESLint EnrollForm

--- imports/client/components/EnrollForm.jsx
+++ imports/client/components/EnrollForm.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { AccountForm } from '/imports/client/components/AccountForm.jsx';
 
 const EnrollForm = React.createClass({
+  propTypes: {
+    params: React.PropTypes.shape({
+      token: React.PropTypes.string.isRequired,
+    }).isRequired,
+  },
+
   render() {
-    return <AccountForm format='enroll' token={this.props.params.token} />;
+    return <AccountForm format="enroll" token={this.props.params.token} />;
   },
 });
 
