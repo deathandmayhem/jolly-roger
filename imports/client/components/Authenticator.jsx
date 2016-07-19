976c14aabfbf2efbf88679a31e495443a9366e0c
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-19 01:16:37 -0700

    ESLint Authenticator

--- imports/client/components/Authenticator.jsx
+++ imports/client/components/Authenticator.jsx
@@ -1,30 +1,37 @@
 import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
 import React from 'react';
 import { browserHistory } from 'react-router';
-// TODO: ReactMeteorData
+import { ReactMeteorData } from 'meteor/react-meteor-data';
 
 const Authenticator = React.createClass({
+  propTypes: {
+    route: React.PropTypes.object,
+    children: React.PropTypes.node,
+    location: React.PropTypes.object,
+  },
+
   mixins: [ReactMeteorData],
 
+  componentWillMount() {
+    this.checkAuth();
+  },
+
+  componentDidUpdate() {
+    this.checkAuth();
+  },
+
   getMeteorData() {
-    return {user: Meteor.user()};
+    return { user: Meteor.user() };
   },
 
   checkAuth() {
     if (!this.data.user && this.props.route.authenticated) {
       browserHistory.replaceState(_.pick(this.props.location, 'pathname', 'query'), '/login');
     } else if (this.data.user && !this.props.route.authenticated) {
-      const state = _.extend({path: '/', query: undefined}, this.props.location.state);
+      const state = _.extend({ path: '/', query: undefined }, this.props.location.state);
       browserHistory.replaceState(null, state.pathname, state.query);
     }
-  },
-
-  componentWillMount() {
-    this.checkAuth();
-  },
-
-  componentDidUpdate(_prevProps, _prevState) {
-    this.checkAuth();
   },
 
   render() {
