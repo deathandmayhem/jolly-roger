26c0cd557c5bc2afb90bc380dbd921ebaa79569f
Drew Fisher <drew.m.fisher@gmail.com>
2016-07-19 01:27:59 -0700

    ESLint HuntApp

--- imports/client/components/HuntApp.jsx
+++ imports/client/components/HuntApp.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { JRPropTypes } from '/imports/client/JRPropTypes.js';
 import { HuntMembershipVerifier } from '/imports/client/components/HuntMembershipVerifier.jsx';
-// TODO: ReactMeteorData
+import { ReactMeteorData } from 'meteor/react-meteor-data';
 
 const HuntApp = React.createClass({
-  mixins: [ReactMeteorData],
+  propTypes: {
+    params: React.PropTypes.shape({
+      huntId: React.PropTypes.string.isRequired,
+    }).isRequired,
+    children: React.PropTypes.node,
+  },
 
   contextTypes: {
     subs: JRPropTypes.subs,
   },
 
+  mixins: [ReactMeteorData],
+
   getMeteorData() {
-    this.context.subs.subscribe('mongo.hunts', {_id: this.props.params.huntId});
-    return {hunt: Models.Hunts.findOne(this.props.params.huntId)};
+    this.context.subs.subscribe('mongo.hunts', { _id: this.props.params.huntId });
+    return { hunt: Models.Hunts.findOne(this.props.params.huntId) };
   },
 
   render() {
