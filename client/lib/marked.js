f6fe2d1e4198f1725ad8517caef6b844fcea70c3
Evan Broder <evan@ebroder.net>
2016-12-01 19:24:41 -0800

    Fix a new regex lint failure

--- client/lib/marked.js
+++ client/lib/marked.js
@@ -1,7 +1,7 @@
 import marked from 'marked';
 
 marked.InlineLexer.rules.gfm.em = /^\b_((?:__|[^_])+?)_\b/;
-marked.InlineLexer.rules.gfm.strong = /^\*\b((?:\*\*|[^\*])+?)\b\*/;
+marked.InlineLexer.rules.gfm.strong = /^\*\b((?:\*\*|[^*])+?)\b\*/;
 
 const renderer = new class extends marked.Renderer {
   link(href, title, link) {
