09c495905c0bd875f37348070c1b14f8ce0782ff
Evan Broder <evan@ebroder.net>
2016-12-26 17:02:05 -0800

    Consolidate the various helper components for HuntApp

--- imports/client/components/HuntApp.jsx
+++ imports/client/components/HuntApp.jsx
@@ -1,8 +1,116 @@
+import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
 import React from 'react';
+import BS from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 import { JRPropTypes } from '/imports/client/JRPropTypes.js';
-import { HuntMembershipVerifier } from '/imports/client/components/HuntMembershipVerifier.jsx';
 import { ReactMeteorData } from 'meteor/react-meteor-data';
+import marked from 'marked';
+
+const HuntMemberError = React.createClass({
+  propTypes: {
+    huntId: React.PropTypes.string.isRequired,
+  },
+
+  contextTypes: {
+    router: React.PropTypes.object.isRequired,
+    subs: JRPropTypes.subs,
+  },
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    const handle = this.context.subs.subscribe('mongo.hunts', { _id: this.props.huntId });
+    return {
+      ready: handle.ready(),
+      hunt: Models.Hunts.findOne(this.props.huntId),
+      canJoin: Roles.userHasPermission(Meteor.userId(), 'hunt.join', this.props.huntId),
+    };
+  },
+
+  join() {
+    Meteor.call('addToHunt', this.props.huntId, Meteor.user().emails[0].address);
+  },
+
+  joinButton() {
+    if (this.data.canJoin) {
+      return (
+        <BS.Button bsStyle="primary" onClick={this.join}>
+          Use operator permissions to join
+        </BS.Button>
+      );
+    }
+    return null;
+  },
+
+  render() {
+    if (!this.data.ready) {
+      return <span>loading...</span>;
+    }
+
+    const msg = marked(this.data.hunt.signupMessage || '', { sanitize: true });
+    return (
+      <div>
+        <BS.Alert bsStyle="warning">
+          You're not signed up for this hunt ({this.data.hunt.name}) yet.
+        </BS.Alert>
+
+        <div dangerouslySetInnerHTML={{ __html: msg }} />
+
+        <BS.ButtonToolbar>
+          <BS.Button bsStyle="default" onClick={this.context.router.goBack}>
+            Whoops! Get me out of here
+          </BS.Button>
+          {this.joinButton()}
+        </BS.ButtonToolbar>
+      </div>
+    );
+  },
+});
+
+const HuntMembershipVerifier = React.createClass({
+  propTypes: {
+    huntId: React.PropTypes.string.isRequired,
+    children: React.PropTypes.node,
+  },
+
+  contextTypes: {
+    subs: JRPropTypes.subs,
+  },
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    const userHandle = this.context.subs.subscribe('huntMembership');
+    if (!userHandle.ready()) {
+      return { ready: false };
+    }
+
+    const user = Meteor.user();
+    if (!user) {
+      return { ready: false };
+    }
+
+    if (!_.contains(user.hunts, this.props.huntId)) {
+      return {
+        member: false,
+        ready: true,
+      };
+    }
+
+    return { ready: true, member: true };
+  },
+
+  render() {
+    if (!this.data.ready) {
+      return <span>loading...</span>;
+    } else if (!this.data.member) {
+      return <HuntMemberError huntId={this.props.huntId} />;
+    } else {
+      return React.Children.only(this.props.children);
+    }
+  },
+});
 
 const HuntApp = React.createClass({
   propTypes: {
