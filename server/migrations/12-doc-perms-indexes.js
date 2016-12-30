156f63bec207390491353c99b7cdd08b4b0819b5
Evan Broder <evan@ebroder.net>
2016-12-30 15:00:05 -0800

    Only grant permissions when we haven't before
    Otherwise, we're going to get the shit ratelimited out of us by
    Google.

---
+++ server/migrations/12-doc-perms-indexes.js
@@ -0,0 +1,11 @@
+import { Migrations } from 'meteor/percolate:migrations';
+
+Migrations.add({
+  version: 12,
+  name: 'Add indexes for document permissions',
+  up() {
+    Models.DocumentPermissions._ensureIndex(
+      { document: 1, user: 1, googleAccount: 1 },
+      { unique: true });
+  },
+});
