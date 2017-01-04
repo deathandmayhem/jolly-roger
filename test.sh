b4ac07935940040845d5eae18ff3aab31b57bbf4
Evan Broder <evan@ebroder.net>
2017-01-04 15:28:52 -0800

    Configure Circle CI for running linting

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+PACKAGES="$(find -name node_modules -prune -or -name .meteor -prune -or -name package.json -print | sed -e 's,/package.json,,g')"
+
+for pkg in $PACKAGES; do
+    (cd "$pkg" && npm run lint)
+done
