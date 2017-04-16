3867920fdd5c9c6dd5d272fe9e01600bc8f8f348
Evan Broder <evan@ebroder.net>
2017-04-16 15:54:33 +0100

    Fix Docker run script when keytab is missing
    In case you're running Jolly Roger without the Kerberos/moira
    integration.

--- scripts/run_jolly_roger.sh
+++ scripts/run_jolly_roger.sh
@@ -32,4 +32,8 @@ export GIT_REVISION="$(cat /built_app/GIT_REVISION)"
 
 credstash get krb5.keytab | openssl base64 -d > /krb5.keytab
 
-exec k5start -U -f /krb5.keytab -- node main.js
+if [ -f /krb5.keytab ]; then
+   exec k5start -U -f /krb5.keytab -- node main.js
+else
+    exec node main.js
+fi
