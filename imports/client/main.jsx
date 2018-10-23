309e3c261780d6b59c1be79a49fdf6387e21655e
Drew Fisher <drew.m.fisher@gmail.com>
2018-10-22 22:09:18 -0700

    Try adding `<meta>` tags to `<head>` dynamically
    We appear to have lost the mobile behavior that restricts width to the actual
    device width.  I suspect this is because we lost the meta tags that were once
    from head.html.
    
    Try adding them dynamically with JS and see what browsers do.  It's not obvious
    to me if under Blaze this would have been compiled into the initial HTML
    fetched or if it would have dynamically added it like I'm doing here.

--- imports/client/main.jsx
+++ imports/client/main.jsx
@@ -4,6 +4,23 @@ import ReactDOM from 'react-dom';
 import Routes from './components/Routes.jsx';
 
 Meteor.startup(() => {
+  // Set up some meta elements previously from head.html
+  // <meta charset="utf-8">
+  const metaCharset = document.createElement('meta');
+  metaCharset.setAttribute('charset', 'utf-8');
+  // <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  const metaIEEdge = document.createElement('meta');
+  metaIEEdge.setAttribute('http-equiv', 'X-UA-Compatible');
+  metaIEEdge.setAttribute('content', 'IE=edge');
+  // <meta name="viewport" content="width=device-width, initial-scale=1">
+  const metaViewport = document.createElement('meta');
+  metaViewport.setAttribute('name', 'viewport');
+  metaViewport.setAttribute('content', 'width=device-width, initial-scale=1');
+  document.head.appendChild(metaCharset);
+  document.head.appendChild(metaIEEdge);
+  document.head.appendChild(metaViewport);
+
+  // Add a div under <body> and mount all the React stuff upon it.
   const container = document.createElement('div');
   document.body.appendChild(container);
   ReactDOM.render(<Routes />, container);
