2fe873b9be97c1c8b607bf681870a554926c2b19
Drew Fisher <drew.m.fisher@gmail.com>
2018-10-29 20:47:04 -0700

    Publish Settings to admins

--- imports/lib/models/settings.js
+++ imports/lib/models/settings.js
@@ -4,4 +4,14 @@ import SettingsSchema from '../schemas/settings.js';
 const Settings = new Base('settings');
 Settings.attachSchema(SettingsSchema);
 
+function queryModifier(q) {
+  // Only allow admins to pull down Settings.
+  if (Roles.userHasRole(this.userId, 'admin')) {
+    return q;
+  }
+
+  // Make the query evaluate to nothing
+  return { $and: [false, q] };
+}
+Settings.publish(queryModifier);
 export default Settings;
