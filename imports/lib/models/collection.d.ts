130d9acc76a75c25c7bd7631ac130f00521e625c
Evan <evan@ebroder.net>
2019-04-14 13:52:16 -0700

    Convert Base model class to Typescript

---
+++ imports/lib/models/collection.d.ts
@@ -0,0 +1,10 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+declare module 'meteor/mongo' {
+  module Mongo {
+    interface Collection<T> {
+      attachRoles(rolesPrefix: string): void;
+    }
+  }
+}
