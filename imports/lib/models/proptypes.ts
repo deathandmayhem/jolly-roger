af926d0c34569f4d30a005f98b972b4ae2f44cf7
Evan <evan@ebroder.net>
2019-06-29 17:20:35 -0700

    Convert asReactPropTypes to Typescript
    asReactPropTypes returns something that can be passed to
    PropTypes.shape, but we don't have enough type information to figure out
    what that is, so for Typescript code we require people to pass in the
    type that we're converting, and we attempt to represent that to the type
    system.

--- imports/lib/models/proptypes.ts
+++ imports/lib/models/proptypes.ts
@@ -1,7 +1,26 @@
-import PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import SimpleSchema from 'simpl-schema';
 
-const mapKeyToReactPropTypes = function (schema, key, type) {
+declare module 'simpl-schema' {
+  // eslint-disable-next-line import/prefer-default-export
+  export interface SimpleSchema {
+    // asReactPropTypes returns something that can be passed to PropTypes.shape,
+    // but we don't have enough type information to figure out what that is, so
+    // for Typescript code we require people to pass in the type that we're
+    // converting, and we attempt to represent that to the type system.
+    asReactPropTypes<T = never>(): {
+      [K in keyof T]: React.Validator<T[K]>
+    };
+    mergedSchema(): Record<string, any>;
+    getQuickTypeForKey(key: string): string;
+  }
+}
+
+const mapKeyToReactPropTypes = function (
+  schema: SimpleSchema,
+  key: string,
+  type: {optional?: boolean}
+) {
   // Attempts to map the field into a React type
   //
   // In theory we should be able to get more sophisticated shape types for
@@ -57,8 +76,10 @@ const mapKeyToReactPropTypes = function (schema, key, type) {
 // Yay, pseudo-typechecking.
 // Note that this shape is neither complete nor as expressive as the schema.
 // This just makes it handier to use with React.
-SimpleSchema.prototype.asReactPropTypes = function asReactPropTypes() {
-  const pt = {};
+SimpleSchema.prototype.asReactPropTypes = function asReactPropTypes(
+  this: SimpleSchema
+): Record<string, React.Validator<any>> {
+  const pt: Record<string, React.Validator<any>> = {};
   const schema = this.mergedSchema();
   Object.keys(schema).forEach((key) => {
     // Skip inner references to array or object members - these will be processed when looking at
@@ -68,7 +89,9 @@ SimpleSchema.prototype.asReactPropTypes = function asReactPropTypes() {
     }
 
     const reactType = mapKeyToReactPropTypes(this, key, schema[key]);
-    pt[key] = reactType;
+    if (reactType) {
+      pt[key] = reactType;
+    }
   });
 
   return pt;
