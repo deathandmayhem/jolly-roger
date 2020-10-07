ce6ff267d7a016de04a0b40aad2539d5067b8932
Evan <evan@ebroder.net>
2020-10-07 15:30:54 -0600

    Move BrowserRouter out of Routes
    For testing, we want to be able to override the router so that we can
    drive it.

--- imports/client/main.tsx
+++ imports/client/main.tsx
@@ -1,11 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes';
 
 Meteor.startup(() => {
   const container = document.createElement('div');
   container.className = 'jolly-roger';
   document.body.appendChild(container);
-  ReactDOM.render(<Routes />, container);
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>,
+    container
+  );
 });
