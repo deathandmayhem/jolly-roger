11fad5ff80e9a169d48cfc46b630b892c62c0f49
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-10 15:30:59 -0700

    Convert CelebrationCenter to functional component/hooks
    I think this is both easier to read with hooks and more likely actually
    does the right thing now!  I believe the implementation is fixed but
    I've left celebrations statically disabled as a matter of trying not to
    make behavioral changes in this patch series.

--- imports/client/components/CelebrationCenter.tsx
+++ imports/client/components/CelebrationCenter.tsx
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Tracker } from 'meteor/tracker';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Flags from '../../flags';
 import Profiles from '../../lib/models/profiles';
 import Puzzles from '../../lib/models/puzzles';
@@ -10,8 +10,6 @@ import Celebration from './Celebration';
 
 interface CelebrationCenterProps {
   huntId: string;
-  disabled: boolean;
-  muted: boolean;
 }
 
 interface CelebrationCenterQueueItem {
@@ -21,112 +19,84 @@ interface CelebrationCenterQueueItem {
   title: string;
 }
 
-interface CelebrationCenterState {
-  playbackQueue: CelebrationCenterQueueItem[];
-}
+const CelebrationCenter = (props: CelebrationCenterProps) => {
+  const [playbackQueue, setPlaybackQueue] = useState<CelebrationCenterQueueItem[]>([]);
 
-class CelebrationCenter extends React.Component<CelebrationCenterProps, CelebrationCenterState> {
-  private computation?: Tracker.Computation;
+  const tracker = useTracker(() => {
+    // This should be effectively a noop, since we're already fetching it for every hunt
+    Meteor.subscribe('mongo.puzzles', { hunt: props.huntId });
 
-  static displayName = 'CelebrationCenter';
+    const profile = Profiles.findOne({ _id: Meteor.userId()! });
+    const muted = !!(profile && profile.muteApplause);
 
-  constructor(props: CelebrationCenterProps) {
-    super(props);
-    this.state = {
-      playbackQueue: [],
+    return {
+      disabled: Flags.active('disable.applause'),
+      muted,
     };
-  }
+  }, [props.huntId]);
 
-  componentDidMount() {
-    setTimeout(() => this.resetComputation(), 0);
-  }
-
-  componentDidUpdate() {
-    setTimeout(() => this.resetComputation(), 0);
-  }
-
-  componentWillUnmount() {
-    if (this.computation) {
-      this.computation.stop();
-      this.computation = undefined;
-    }
-  }
-
-  onPuzzleSolved = (_puzzle: PuzzleType) => {
+  const onPuzzleSolved = useCallback((puzzle: PuzzleType, newAnswer: string) => {
     // Only celebrate if:
     // 1) we're not on mobile, and
-    // 2) the feature flag is not disabled, and
+    // 2) the global feature flag is not disabled, and
     // 3) TODO: the user has not disabled it in their profile settings
     // Hack: disabled celebrations because I don't want to think about it right now
-    /*
-    const answer = puzzle.answer;
-    if ((window.orientation === undefined) && !this.props.disabled && answer) {
-      this.setState((prevState) => {
-        const newQueue = prevState.playbackQueue.concat([{
+    // eslint-disable-next-line no-constant-condition
+    if ((window.orientation === undefined) && !tracker.disabled && false) {
+      setPlaybackQueue((prevPlaybackQueue) => {
+        const newQueue = prevPlaybackQueue.concat([{
           puzzleId: puzzle._id,
           url: `/hunts/${puzzle.hunt}/puzzles/${puzzle._id}`,
-          answer,
+          answer: newAnswer,
           title: puzzle.title,
         }]);
 
-        return { playbackQueue: newQueue };
+        return newQueue;
       });
     }
-    */
-  };
+  }, [tracker.disabled]);
 
-  resetComputation = () => {
-    if (this.computation) {
-      this.computation.stop();
-    }
-
-    this.computation = Tracker.autorun(() => {
+  useEffect(() => {
+    const puzzleWatcher = Tracker.autorun(() => {
       Puzzles.find().observe({
         changed: (newDoc, oldDoc) => {
           if (oldDoc.answers.length < newDoc.answers.length) {
-            this.onPuzzleSolved(newDoc);
+            const newAnswer = newDoc.answers.find((answer) => !oldDoc.answers.includes(answer));
+            if (newAnswer) {
+              onPuzzleSolved(newDoc, newAnswer);
+            }
           }
         },
       });
     });
-  };
 
-  dismissCurrentCelebration = () => {
-    this.setState((prevState) => {
-      return { playbackQueue: prevState.playbackQueue.slice(1) };
+    return () => {
+      // Clean up the Tracker autorun
+      puzzleWatcher.stop();
+    };
+  }, [onPuzzleSolved]);
+
+  const dismissCurrentCelebration = useCallback(() => {
+    setPlaybackQueue((prevPlaybackQueue) => {
+      return prevPlaybackQueue.slice(1);
     });
-  };
+  }, []);
 
-  render() {
-    if (this.state.playbackQueue.length === 0) {
-      return null;
-    } else {
-      const celebration = this.state.playbackQueue[0];
-      return (
-        <Celebration
-          key={celebration.puzzleId}
-          url={celebration.url}
-          title={celebration.title}
-          answer={celebration.answer}
-          playAudio={!this.props.muted}
-          onClose={this.dismissCurrentCelebration}
-        />
-      );
-    }
+  if (playbackQueue.length === 0) {
+    return null;
+  } else {
+    const celebration = playbackQueue[0];
+    return (
+      <Celebration
+        key={celebration.puzzleId}
+        url={celebration.url}
+        title={celebration.title}
+        answer={celebration.answer}
+        playAudio={!tracker.muted}
+        onClose={dismissCurrentCelebration}
+      />
+    );
   }
-}
+};
 
-const CelebrationCenterContainer = withTracker(({ huntId }: { huntId: string }) => {
-  // This should be effectively a noop, since we're already fetching it for every hunt
-  Meteor.subscribe('mongo.puzzles', { hunt: huntId });
-
-  const profile = Profiles.findOne({ _id: Meteor.userId()! });
-  const muted = !!(profile && profile.muteApplause);
-
-  return {
-    disabled: Flags.active('disable.applause'),
-    muted,
-  };
-})(CelebrationCenter);
-
-export default CelebrationCenterContainer;
+export default CelebrationCenter;
