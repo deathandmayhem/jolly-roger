e49d75b6a7736178c5e615610b54a16817812e64
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-10 14:36:27 -0700

    Convert CallSection to function component/hooks

--- imports/client/components/CallSection.tsx
+++ imports/client/components/CallSection.tsx
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons/faCaretRight';
 import { faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons/faMicrophoneSlash';
 import { faVolumeMute } from '@fortawesome/free-solid-svg-icons/faVolumeMute';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classnames from 'classnames';
-import React from 'react';
+import React, { useCallback } from 'react';
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
@@ -20,7 +20,7 @@ import { RTCConfigType, RTCConfig } from '../rtc_config';
 import CallLinkBox from './CallLinkBox';
 import Spectrum from './Spectrum';
 
-interface RTCCallSectionParams {
+interface RTCCallSectionProps {
   huntId: string;
   puzzleId: string;
   tabId: string;
@@ -35,7 +35,7 @@ interface RTCCallSectionParams {
   onToggleCallersExpanded(): void;
 }
 
-interface RTCCallSectionProps extends RTCCallSectionParams {
+interface RTCCallSectionTrackerData {
   rtcConfigReady: boolean;
   rtcConfig: RTCConfigType | undefined;
   participantsReady: boolean;
@@ -47,185 +47,178 @@ interface RTCCallSectionProps extends RTCCallSectionParams {
   spectraDisabled: boolean;
 }
 
-class RTCCallSection extends React.Component<RTCCallSectionProps> {
-  nonSelfParticipants = () => {
-    return this.props.participants.filter((p) => {
-      return (p.createdBy !== this.props.selfUserId) || (p.tab !== this.props.tabId);
+const RTCCallSection = (props: RTCCallSectionProps) => {
+  const tracker: RTCCallSectionTrackerData = useTracker(() => {
+    const joinSub = Meteor.subscribe('call.join', props.huntId, props.puzzleId, props.tabId);
+    const participants = joinSub.ready() ? CallParticipants.find({
+      hunt: props.huntId,
+      call: props.puzzleId,
+    }).fetch() : [];
+
+    const rtcConfigSub = Meteor.subscribe('rtcconfig');
+    const rtcConfig = RTCConfig.findOne('rtcconfig');
+
+    const selfUserId = Meteor.userId() || undefined;
+    const selfProfile = selfUserId ? Profiles.findOne(selfUserId) : undefined;
+    const selfParticipant = participants.find((p) => {
+      return p.createdBy === selfUserId && p.tab === props.tabId;
     });
-  }
+    let signalsReady;
+    if (selfParticipant) {
+      const signalsSub = Meteor.subscribe('call.signal', selfParticipant._id);
+      signalsReady = signalsSub.ready();
+    } else {
+      signalsReady = false;
+    }
 
-  toggleMuted = () => {
-    this.props.onToggleMute();
-  };
+    const spectraDisabled = Flags.active('disable.spectra');
 
-  toggleDeafened = () => {
-    this.props.onToggleDeafen();
-  };
+    return {
+      rtcConfigReady: rtcConfigSub.ready(),
+      rtcConfig,
+      participantsReady: joinSub.ready(),
+      selfParticipant,
+      selfProfile,
+      participants,
+      signalsReady,
+      selfUserId,
+      spectraDisabled,
+    };
+  }, [props.huntId, props.puzzleId, props.tabId]);
 
-  leaveCall = () => {
-    this.props.onLeaveCall();
-  };
+  const nonSelfParticipants = useCallback(() => {
+    return tracker.participants.filter((p) => {
+      return (p.createdBy !== tracker.selfUserId) || (p.tab !== props.tabId);
+    });
+  }, [tracker.participants, tracker.selfUserId, props.tabId]);
 
-  renderSelfBox = () => {
-    const selfProfile = this.props.selfProfile;
-    const discordAccount = selfProfile && selfProfile.discordAccount;
-    const discordAvatarUrl = discordAccount && getAvatarCdnUrl(discordAccount);
-    const initial = selfProfile ? selfProfile.displayName.slice(0, 1) : 'U'; // get it?  it's you
-    return (
-      <OverlayTrigger
+  const toggleMuted = useCallback(() => {
+    props.onToggleMute();
+  }, [props.onToggleMute]);
+
+  const toggleDeafened = useCallback(() => {
+    props.onToggleDeafen();
+  }, [props.onToggleDeafen]);
+
+  const leaveCall = useCallback(() => {
+    props.onLeaveCall();
+  }, [props.onLeaveCall]);
+
+  const selfProfile = tracker.selfProfile;
+  const discordAccount = selfProfile && selfProfile.discordAccount;
+  const discordAvatarUrl = discordAccount && getAvatarCdnUrl(discordAccount);
+  const initial = selfProfile ? selfProfile.displayName.slice(0, 1) : 'U'; // get it?  it's you
+  const selfBox = (
+    <OverlayTrigger
+      key="self"
+      placement="right"
+      overlay={(
+        <Tooltip id="caller-self">
+          <div>You are in the call.</div>
+          {props.muted && <div>You are currently muted and will transmit no audio.</div>}
+          {props.deafened && <div>You are currently deafened and will hear no audio.</div>}
+        </Tooltip>
+      )}
+    >
+      <div
         key="self"
-        placement="right"
-        overlay={(
-          <Tooltip id="caller-self">
-            <div>You are in the call.</div>
-            {this.props.muted && <div>You are currently muted and will transmit no audio.</div>}
-            {this.props.deafened && <div>You are currently deafened and will hear no audio.</div>}
-          </Tooltip>
-        )}
+        className={classnames('people-item', {
+          muted: props.muted,
+          deafened: props.deafened,
+          live: !props.muted && !props.deafened,
+        })}
       >
-        <div
-          key="self"
-          className={classnames('people-item', {
-            muted: this.props.muted,
-            deafened: this.props.deafened,
-            live: !this.props.muted && !this.props.deafened,
-          })}
-        >
-          {discordAvatarUrl ? (
-            <img
-              alt="Your own Discord avatar"
-              className="discord-avatar"
-              src={discordAvatarUrl}
-            />
-          ) : (
-            <span className="initial">{initial}</span>
-          )}
-          <div className="webrtc">
-            {this.props.muted && <span className="icon muted-icon"><FontAwesomeIcon icon={faMicrophoneSlash} /></span>}
-            {this.props.deafened && <span className="icon deafened-icon"><FontAwesomeIcon icon={faVolumeMute} /></span>}
-            {!this.props.spectraDisabled && !this.props.muted && !this.props.deafened ? (
-              <Spectrum
-                className="spectrogram"
-                width={40}
-                height={40}
-                audioContext={this.props.audioContext}
-                ref={((spectrum) => {
-                  if (spectrum) {
-                    spectrum.connect(this.props.localStream);
-                  }
+        {discordAvatarUrl ? (
+          <img
+            alt="Your own Discord avatar"
+            className="discord-avatar"
+            src={discordAvatarUrl}
+          />
+        ) : (
+          <span className="initial">{initial}</span>
+        )}
+        <div className="webrtc">
+          {props.muted && <span className="icon muted-icon"><FontAwesomeIcon icon={faMicrophoneSlash} /></span>}
+          {props.deafened && <span className="icon deafened-icon"><FontAwesomeIcon icon={faVolumeMute} /></span>}
+          {!tracker.spectraDisabled && !props.muted && !props.deafened ? (
+            <Spectrum
+              className="spectrogram"
+              width={40}
+              height={40}
+              audioContext={props.audioContext}
+              ref={((spectrum) => {
+                if (spectrum) {
+                  spectrum.connect(props.localStream);
                 }
-                )}
-              />
-            ) : null}
-          </div>
+              }
+              )}
+            />
+          ) : null}
         </div>
-      </OverlayTrigger>
-    );
-  };
+      </div>
+    </OverlayTrigger>
+  );
 
-  render() {
-    if (!this.props.rtcConfigReady || !this.props.participantsReady || !this.props.signalsReady) {
-      return <div />;
-    }
+  if (!tracker.rtcConfigReady || !tracker.participantsReady || !tracker.signalsReady) {
+    return <div />;
+  }
 
-    if (!this.props.rtcConfig) {
-      return (
-        <div>
-          WebRTC misconfigured on the server.  Contact your server administrator.
-        </div>
-      );
-    }
-
-    const callerCount = this.props.participants.length;
-    const others = this.nonSelfParticipants();
-
-    const callersHeaderIcon = this.props.callersExpanded ? faCaretDown : faCaretRight;
-
+  if (!tracker.rtcConfig) {
     return (
-      <>
-        <div className="av-actions">
-          <Button
-            variant={this.props.muted ? 'secondary' : 'light'}
-            size="sm"
-            onClick={this.toggleMuted}
-          >
-            {this.props.muted ? 'unmute' : 'mute self'}
-          </Button>
-          <Button
-            variant={this.props.deafened ? 'secondary' : 'light'}
-            size="sm"
-            onClick={this.toggleDeafened}
-          >
-            {this.props.deafened ? 'undeafen' : 'deafen self'}
-          </Button>
-          <Button variant="danger" size="sm" onClick={this.leaveCall}>leave call</Button>
-        </div>
-        <div className="chatter-subsection av-chatters">
-          <header onClick={this.props.onToggleCallersExpanded}>
-            <FontAwesomeIcon fixedWidth icon={callersHeaderIcon} />
-            {`${callerCount} caller${callerCount !== 1 ? 's' : ''}`}
-          </header>
-          <div className={classnames('people-list', { collapsed: !this.props.callersExpanded })}>
-            {this.renderSelfBox()}
-            {this.props.signalsReady && this.props.selfParticipant && others.map((p) => {
-              return (
-                <CallLinkBox
-                  key={p._id}
-                  rtcConfig={this.props.rtcConfig!}
-                  selfParticipant={this.props.selfParticipant!}
-                  peerParticipant={p}
-                  localStream={this.props.localStream}
-                  audioContext={this.props.audioContext}
-                  deafened={this.props.deafened}
-                />
-              );
-            })}
-          </div>
-        </div>
-      </>
+      <div>
+        WebRTC misconfigured on the server.  Contact your server administrator.
+      </div>
     );
   }
-}
 
-// This exists just to apply React lifecycle treatment to this subscribe.
-const tracker = withTracker((params: RTCCallSectionParams) => {
-  const joinSub = Meteor.subscribe('call.join', params.huntId, params.puzzleId, params.tabId);
-  const participants = joinSub.ready() ? CallParticipants.find({
-    hunt: params.huntId,
-    call: params.puzzleId,
-  }).fetch() : [];
+  const callerCount = tracker.participants.length;
+  const others = nonSelfParticipants();
 
-  const rtcConfigSub = Meteor.subscribe('rtcconfig');
-  const rtcConfig = RTCConfig.findOne('rtcconfig');
+  const callersHeaderIcon = props.callersExpanded ? faCaretDown : faCaretRight;
 
-  const selfUserId = Meteor.userId() || undefined;
-  const selfProfile = selfUserId ? Profiles.findOne(selfUserId) : undefined;
-  const selfParticipant = participants.find((p) => {
-    return p.createdBy === selfUserId && p.tab === params.tabId;
-  });
-  let signalsReady;
-  if (selfParticipant) {
-    const signalsSub = Meteor.subscribe('call.signal', selfParticipant._id);
-    signalsReady = signalsSub.ready();
-  } else {
-    signalsReady = false;
-  }
+  return (
+    <>
+      <div className="av-actions">
+        <Button
+          variant={props.muted ? 'secondary' : 'light'}
+          size="sm"
+          onClick={toggleMuted}
+        >
+          {props.muted ? 'unmute' : 'mute self'}
+        </Button>
+        <Button
+          variant={props.deafened ? 'secondary' : 'light'}
+          size="sm"
+          onClick={toggleDeafened}
+        >
+          {props.deafened ? 'undeafen' : 'deafen self'}
+        </Button>
+        <Button variant="danger" size="sm" onClick={leaveCall}>leave call</Button>
+      </div>
+      <div className="chatter-subsection av-chatters">
+        <header onClick={props.onToggleCallersExpanded}>
+          <FontAwesomeIcon fixedWidth icon={callersHeaderIcon} />
+          {`${callerCount} caller${callerCount !== 1 ? 's' : ''}`}
+        </header>
+        <div className={classnames('people-list', { collapsed: !props.callersExpanded })}>
+          {selfBox}
+          {tracker.signalsReady && tracker.selfParticipant && others.map((p) => {
+            return (
+              <CallLinkBox
+                key={p._id}
+                rtcConfig={tracker.rtcConfig!}
+                selfParticipant={tracker.selfParticipant!}
+                peerParticipant={p}
+                localStream={props.localStream}
+                audioContext={props.audioContext}
+                deafened={props.deafened}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-  const spectraDisabled = Flags.active('disable.spectra');
-
-  return {
-    rtcConfigReady: rtcConfigSub.ready(),
-    rtcConfig,
-    participantsReady: joinSub.ready(),
-    selfParticipant,
-    selfProfile,
-    participants,
-    signalsReady,
-    selfUserId,
-    spectraDisabled,
-  };
-});
-
-const RTCCallSectionContainer = tracker(RTCCallSection);
-
-export default RTCCallSectionContainer;
+export default RTCCallSection;
