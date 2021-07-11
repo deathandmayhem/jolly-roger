5aeedefdec50cf76878e4fce0001b7546f076ebe
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-10 20:15:41 -0700

    Convert FirstUserForm to function compoennt

--- imports/client/components/FirstUserForm.tsx
+++ imports/client/components/FirstUserForm.tsx
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import FormControl, { FormControlProps } from 'react-bootstrap/FormControl';
@@ -13,68 +13,41 @@ enum SubmitState {
   ERROR = 'error',
 }
 
-interface FirstUserFormProps {
-}
+const FirstUserForm = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [submitState, setSubmitState] = useState<SubmitState>(SubmitState.IDLE);
+  const [submitError, setSubmitError] = useState<string>('');
 
-interface FirstUserFormState {
-  email: string;
-  password: string;
-  submitState: SubmitState;
-  submitError: string;
-}
+  const onEmailChange: FormControlProps['onChange'] = useCallback((e) => {
+    setEmail(e.currentTarget.value);
+  }, []);
+  const onPasswordChange: FormControlProps['onChange'] = useCallback((e) => {
+    setPassword(e.currentTarget.value);
+  }, []);
+  const dismissAlert = useCallback(() => {
+    setSubmitState(SubmitState.IDLE);
+  }, []);
 
-class FirstUserForm extends React.Component<FirstUserFormProps, FirstUserFormState> {
-  constructor(props: FirstUserFormProps) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      submitState: SubmitState.IDLE,
-      submitError: '',
-    };
-  }
-
-  onEmailChange: FormControlProps['onChange'] = (e) => {
-    this.setState({
-      email: e.currentTarget.value,
-    });
-  };
-
-  onPasswordChange: FormControlProps['onChange'] = (e) => {
-    this.setState({
-      password: e.currentTarget.value,
-    });
-  };
-
-  onSubmit = (e: React.FormEvent<any>) => {
+  const onSubmit = useCallback((e: React.FormEvent<any>) => {
     e.preventDefault();
 
-    const email = this.state.email.trim();
-    const password = this.state.password;
-
-    if (email.length === 0 || password.length === 0) {
-      this.setState({
-        submitState: SubmitState.ERROR,
-        submitError: 'Must provide both an email and password',
-      });
+    const trimmedEmail = email.trim();
+    if (trimmedEmail.length === 0 || password.length === 0) {
+      setSubmitError('Must provide both an email and password');
+      setSubmitState(SubmitState.ERROR);
     } else {
-      this.setState({
-        submitState: SubmitState.SUBMITTING,
-      });
-      Meteor.call('provisionFirstUser', email, password, (err?: Error) => {
+      setSubmitState(SubmitState.SUBMITTING);
+      Meteor.call('provisionFirstUser', trimmedEmail, password, (err?: Error) => {
         if (err) {
-          this.setState({
-            submitState: SubmitState.ERROR,
-            submitError: err.message,
-          });
+          setSubmitError(err.message);
+          setSubmitState(SubmitState.ERROR);
         } else {
           // Success!  Do login.
-          Meteor.loginWithPassword(email, password, (error?: Error) => {
+          Meteor.loginWithPassword(trimmedEmail, password, (error?: Error) => {
             if (error) {
-              this.setState({
-                submitState: SubmitState.ERROR,
-                submitError: error.message,
-              });
+              setSubmitError(error.message);
+              setSubmitState(SubmitState.ERROR);
             } else {
               // We've logged in. We should get redirected to /hunts by the
               // AuthenticatedRoute that's rendering us.
@@ -83,59 +56,51 @@ class FirstUserForm extends React.Component<FirstUserFormProps, FirstUserFormSta
         }
       });
     }
-  };
+  }, [email, password]);
 
-  dismissAlert = () => {
-    this.setState({
-      submitState: SubmitState.IDLE,
-    });
-  };
-
-  render() {
-    const shouldDisableForm = this.state.submitState === SubmitState.SUBMITTING;
-    return (
-      <form onSubmit={this.onSubmit}>
-        <h1>Create first user</h1>
-        <p>This user will have server admin and operator privileges.</p>
-        {this.state.submitState === SubmitState.SUBMITTING ? <Alert variant="info">Creating first user...</Alert> : null}
-        {this.state.submitState === SubmitState.SUCCESS ? <Alert variant="success" dismissible onClose={this.dismissAlert}>Created user.</Alert> : null}
-        {this.state.submitState === SubmitState.ERROR ? (
-          <Alert variant="danger" dismissible onClose={this.dismissAlert}>
-            Saving failed:
-            {' '}
-            {this.state.submitError}
-          </Alert>
-        ) : null}
-        <FormGroup>
-          <FormLabel htmlFor="jr-first-user-email">
-            Email
-          </FormLabel>
-          <FormControl
-            id="jr-first-user-email"
-            type="text"
-            value={this.state.email}
-            disabled={shouldDisableForm}
-            onChange={this.onEmailChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <FormLabel htmlFor="jr-first-user-password">
-            Password
-          </FormLabel>
-          <FormControl
-            id="jr-first-user-password"
-            type="password"
-            value={this.state.password}
-            disabled={shouldDisableForm}
-            onChange={this.onPasswordChange}
-          />
-        </FormGroup>
-        <Button variant="primary" type="submit" disabled={shouldDisableForm} onSubmit={this.onSubmit}>
-          Create first user
-        </Button>
-      </form>
-    );
-  }
-}
+  const shouldDisableForm = submitState === SubmitState.SUBMITTING;
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Create first user</h1>
+      <p>This user will have server admin and operator privileges.</p>
+      {submitState === SubmitState.SUBMITTING ? <Alert variant="info">Creating first user...</Alert> : null}
+      {submitState === SubmitState.SUCCESS ? <Alert variant="success" dismissible onClose={dismissAlert}>Created user.</Alert> : null}
+      {submitState === SubmitState.ERROR ? (
+        <Alert variant="danger" dismissible onClose={dismissAlert}>
+          Saving failed:
+          {' '}
+          {submitError}
+        </Alert>
+      ) : null}
+      <FormGroup>
+        <FormLabel htmlFor="jr-first-user-email">
+          Email
+        </FormLabel>
+        <FormControl
+          id="jr-first-user-email"
+          type="text"
+          value={email}
+          disabled={shouldDisableForm}
+          onChange={onEmailChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <FormLabel htmlFor="jr-first-user-password">
+          Password
+        </FormLabel>
+        <FormControl
+          id="jr-first-user-password"
+          type="password"
+          value={password}
+          disabled={shouldDisableForm}
+          onChange={onPasswordChange}
+        />
+      </FormGroup>
+      <Button variant="primary" type="submit" disabled={shouldDisableForm} onSubmit={onSubmit}>
+        Create first user
+      </Button>
+    </form>
+  );
+};
 
 export default FirstUserForm;
