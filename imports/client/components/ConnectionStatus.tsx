38a7ccdc3a80449f813a87245e81ba4da7c006d2
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-10 19:05:47 -0700

    Convert ConnectionStatus to function components/hooks
    Split this into two components so that the timer can be bound to an
    effect lifecycle without incurring every-second repaints when connected
    normally.

--- imports/client/components/ConnectionStatus.tsx
+++ imports/client/components/ConnectionStatus.tsx
@@ -1,84 +1,85 @@
 import { DDP } from 'meteor/ddp';
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import React, { useEffect, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Ansible from '../../ansible';
 
-interface ConnectionStatusProps {
-  meteorStatus: DDP.DDPStatus;
+interface WaitingAlertProps {
+  retryTime: DDP.DDPStatus['retryTime'];
 }
 
-class ConnectionStatus extends React.Component<ConnectionStatusProps> {
-  timeoutId?: number;
+const WaitingAlert = (props: WaitingAlertProps) => {
+  const now = Date.now();
+  const retryTime = props.retryTime || now;
+  const [lastUpdated, setLastUpdated] = useState<number>(now);
 
-  refresh() {
-    // Mark this timeout as completed
-    if (this.timeoutId) {
-      this.timeoutId = undefined;
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      setLastUpdated(Date.now());
+    }, 1000);
+
+    return () => {
+      window.clearInterval(timer);
+    };
+  }, []);
+
+  const timeToRetry = Math.ceil((retryTime - lastUpdated) / 1000);
+  return (
+    <Alert variant="warning">
+      We can&apos;t connect to Jolly Roger right now. We&apos;ll try again in
+      {' '}
+      {timeToRetry}
+      s. Your pending changes will be pushed to the server when we reconnect.
+      {' '}
+      <Button variant="link" onClick={Meteor.reconnect}>retry now</Button>
+    </Alert>
+  );
+};
+
+const ConnectionStatus = () => {
+  const meteorStatus: DDP.DDPStatus = useTracker(() => {
+    return Meteor.status();
+  }, []);
+
+  switch (meteorStatus.status) {
+    case 'connecting':
+      return (
+        <Alert variant="warning">
+          Trying to reconnect to Jolly Roger...
+        </Alert>
+      );
+    case 'failed':
+      return (
+        <Alert variant="danger">
+          <strong>Oh no!</strong>
+          {' '}
+          Unable to connect to Jolly Roger:
+          {meteorStatus.reason}
+        </Alert>
+      );
+    case 'waiting': {
+      return (
+        <WaitingAlert retryTime={meteorStatus.retryTime} />
+      );
     }
-    this.forceUpdate();
+    case 'offline':
+      return (
+        <Alert variant="warning">
+          <strong>Warning!</strong>
+          {' '}
+          Currently not connected to Jolly Roger server. Changes will be synced when you
+          reconnect.
+          <Button variant="link" onClick={Meteor.reconnect}>reconnect now</Button>
+        </Alert>
+      );
+    case 'connected':
+      return null;
+    default:
+      Ansible.warn('Unknown connection status', { state: meteorStatus.status });
+      return null;
   }
+};
 
-  render() {
-    switch (this.props.meteorStatus.status) {
-      case 'connecting':
-        return (
-          <Alert variant="warning">
-            Trying to reconnect to Jolly Roger...
-          </Alert>
-        );
-      case 'failed':
-        return (
-          <Alert variant="danger">
-            <strong>Oh no!</strong>
-            {' '}
-            Unable to connect to Jolly Roger:
-            {this.props.meteorStatus.reason}
-          </Alert>
-        );
-      case 'waiting': {
-        const now = Date.now();
-        const retryTime = this.props.meteorStatus.retryTime || now;
-        const timeToRetry = Math.ceil((retryTime - now) / 1000);
-
-        // If we have no refresh scheduled yet, trigger a refresh in a second.
-        if (this.timeoutId === undefined) {
-          this.timeoutId = window.setTimeout(() => this.refresh(), 1000);
-        }
-        return (
-          <Alert variant="warning">
-            We can&apos;t connect to Jolly Roger right now. We&apos;ll try again in
-            {' '}
-            {timeToRetry}
-            s. Your pending changes will be pushed to the server when we reconnect.
-            {' '}
-            <Button variant="link" onClick={Meteor.reconnect}>retry now</Button>
-          </Alert>
-        );
-      }
-      case 'offline':
-        return (
-          <Alert variant="warning">
-            <strong>Warning!</strong>
-            {' '}
-            Currently not connected to Jolly Roger server. Changes will be synced when you
-            reconnect.
-            <Button variant="link" onClick={Meteor.reconnect}>reconnect now</Button>
-          </Alert>
-        );
-      case 'connected':
-        return null;
-      default:
-        Ansible.warn('Unknown connection status', { state: this.props.meteorStatus.status });
-        return null;
-    }
-  }
-}
-
-const ConnectionStatusContainer = withTracker((_props: {}) => {
-  return { meteorStatus: Meteor.status() };
-})(ConnectionStatus);
-
-export default ConnectionStatusContainer;
+export default ConnectionStatus;
