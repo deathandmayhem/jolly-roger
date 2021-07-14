dea88f05718636d3baf3e2a5abce077e49888672
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-14 02:02:10 -0700

    Convert TagEditor to function component/hooks

--- imports/client/components/TagEditor.tsx
+++ imports/client/components/TagEditor.tsx
@@ -1,51 +1,51 @@
-import { withTracker } from 'meteor/react-meteor-data';
-import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import React, { useCallback } from 'react';
 import Creatable from 'react-select/creatable';
 import Tags from '../../lib/models/tags';
 import { PuzzleType } from '../../lib/schemas/puzzles';
 import { TagType } from '../../lib/schemas/tags';
 
-interface TagEditorParams {
+interface TagEditorProps {
   puzzle: PuzzleType;
   onSubmit: (value: string) => void;
   onCancel: () => void;
 }
 
-interface TagEditorProps extends TagEditorParams {
+interface TagEditorTracker {
   allTags: TagType[];
 }
 
-class TagEditor extends React.Component<TagEditorProps> {
-  onBlur = () => {
+const TagEditor = (props: TagEditorProps) => {
+  const tracker = useTracker<TagEditorTracker>(() => {
+    return {
+      allTags: Tags.find({ hunt: props.puzzle.hunt }).fetch(),
+    };
+  }, [props.puzzle.hunt]);
+
+  const onBlur = useCallback(() => {
     // Treat blur as "no I didn't mean to do that".  We may have to change this
     // once we have autocomplete .
-    this.props.onCancel();
-  };
+    props.onCancel();
+  }, [props.onCancel]);
 
-  render() {
-    const options = this.props.allTags
-      .map((t) => t.name)
-      .filter(Boolean)
-      .map((t) => {
-        return { value: t, label: t };
-      });
+  const options = tracker.allTags
+    .map((t) => t.name)
+    .filter(Boolean)
+    .map((t) => {
+      return { value: t, label: t };
+    });
 
-    return (
-      <span className="tag-editor">
-        <Creatable
-          options={options}
-          autoFocus
-          openMenuOnFocus
-          onChange={(v) => this.props.onSubmit((v as {value: string}).value)}
-          onBlur={this.onBlur}
-        />
-      </span>
-    );
-  }
-}
+  return (
+    <span className="tag-editor">
+      <Creatable
+        options={options}
+        autoFocus
+        openMenuOnFocus
+        onChange={(v) => props.onSubmit((v as {value: string}).value)}
+        onBlur={onBlur}
+      />
+    </span>
+  );
+};
 
-const TagEditorContainer = withTracker(({ puzzle }: TagEditorParams) => {
-  return { allTags: Tags.find({ hunt: puzzle.hunt }).fetch() };
-})(TagEditor);
-
-export default TagEditorContainer;
+export default TagEditor;
