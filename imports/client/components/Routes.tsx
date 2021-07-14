57907bab4d41bc97f6e0f410a33cc59f2eef52a2
Drew Fisher <drew.m.fisher@gmail.com>
2021-07-14 00:11:18 -0700

    Convert Routes to function component

--- imports/client/components/Routes.tsx
+++ imports/client/components/Routes.tsx
@@ -16,33 +16,31 @@ import RootRedirector from './RootRedirector';
 import SetupPage from './SetupPage';
 import UnauthenticatedRoute from './UnauthenticatedRoute';
 
-class Routes extends React.PureComponent {
-  render() {
-    return (
-      <DocumentTitle title="Jolly Roger">
-        <BreadcrumbsProvider>
-          <Switch>
-            {/* Index redirect */}
-            <Route exact path="/" component={RootRedirector} />
+const Routes = React.memo(() => {
+  return (
+    <DocumentTitle title="Jolly Roger">
+      <BreadcrumbsProvider>
+        <Switch>
+          {/* Index redirect */}
+          <Route exact path="/" component={RootRedirector} />
 
-            {/* Authenticated routes - if user not logged in, get redirected to /login */}
-            <AuthenticatedRoute path="/hunts/:huntId" component={HuntApp} />
-            <AuthenticatedRoute path="/hunts" component={HuntListPage} />
-            <AuthenticatedRoute path="/users/:userId" component={ProfilePage} />
-            <AuthenticatedRoute path="/users" component={AllProfileListPage} />
-            <AuthenticatedRoute path="/setup" component={SetupPage} />
-            <AuthenticatedRoute path="/rtcdebug" component={RTCDebugPage} />
+          {/* Authenticated routes - if user not logged in, get redirected to /login */}
+          <AuthenticatedRoute path="/hunts/:huntId" component={HuntApp} />
+          <AuthenticatedRoute path="/hunts" component={HuntListPage} />
+          <AuthenticatedRoute path="/users/:userId" component={ProfilePage} />
+          <AuthenticatedRoute path="/users" component={AllProfileListPage} />
+          <AuthenticatedRoute path="/setup" component={SetupPage} />
+          <AuthenticatedRoute path="/rtcdebug" component={RTCDebugPage} />
 
-            {/* Unauthenticated routes - if user already logged in, get redirected to /hunts */}
-            <UnauthenticatedRoute path="/login" component={LoginForm} />
-            <UnauthenticatedRoute path="/reset-password/:token" component={PasswordResetForm} />
-            <UnauthenticatedRoute path="/enroll/:token" component={EnrollForm} />
-            <UnauthenticatedRoute path="/create-first-user" component={FirstUserForm} />
-          </Switch>
-        </BreadcrumbsProvider>
-      </DocumentTitle>
-    );
-  }
-}
+          {/* Unauthenticated routes - if user already logged in, get redirected to /hunts */}
+          <UnauthenticatedRoute path="/login" component={LoginForm} />
+          <UnauthenticatedRoute path="/reset-password/:token" component={PasswordResetForm} />
+          <UnauthenticatedRoute path="/enroll/:token" component={EnrollForm} />
+          <UnauthenticatedRoute path="/create-first-user" component={FirstUserForm} />
+        </Switch>
+      </BreadcrumbsProvider>
+    </DocumentTitle>
+  );
+});
 
 export default Routes;
