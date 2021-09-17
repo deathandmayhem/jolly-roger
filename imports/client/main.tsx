84927107ebd3c280416f906e19b8bf3ecb6390b4
Evan Broder <evan@ebroder.net>
2021-09-16 18:59:27 -0700

    Push CSS reset into main.tsx

--- imports/client/main.tsx
+++ imports/client/main.tsx
@@ -2,16 +2,33 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Routes from './components/Routes';
+
+// As recommended by Paul Irish: https://www.paulirish.com/2012/box-sizing-border-box-ftw/
+const Reset = createGlobalStyle`
+  html {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`;
 
 Meteor.startup(() => {
   const container = document.createElement('div');
   container.className = 'jolly-roger';
   document.body.appendChild(container);
   ReactDOM.render(
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>,
+    <>
+      <Reset />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>,
     container
   );
 });
