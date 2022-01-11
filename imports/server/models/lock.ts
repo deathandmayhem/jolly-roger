e09ee4beadf70aff72151a6fb60c02c6bd02d1dc
Evan Broder <evan@ebroder.net>
2022-01-11 12:45:03 -0800

    Promisify locking logic
    In the process, split the observer monitoring for removal and the
    timeout monitoring for preemption into separate promises (rather than
    having them both resolve a single future), to avoid having them both
    potentially resolve the same promise.

--- imports/server/models/lock.ts
+++ imports/server/models/lock.ts
@@ -2,11 +2,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { NpmModuleMongodb } from 'meteor/npm-mongo';
+import { Promise as MeteorPromise } from 'meteor/promise';
 import Ansible from '../../ansible';
 import LockSchema, { LockType } from '../schemas/lock';
-
-declare const Npm: any;
-const Future: typeof import('fibers/future') = Npm.require('fibers/future');
 
 // 10 seconds
 export const PREEMPT_TIMEOUT = 10000;
@@ -65,12 +63,12 @@ const Locks = new class extends Mongo.Collection<LockType> {
 
         // Setup the watch now so we don't race between when we check
         // for the lock and when we wait for preemption
-        const removed = new Future<LockType | undefined>();
-        handle = cursor.observeChanges({
-          removed() {
-            cleanupWatches();
-            removed.return(undefined);
-          },
+        const removed = new Promise<undefined>((resolve) => {
+          handle = cursor.observeChanges({
+            removed() {
+              resolve(undefined);
+            },
+          });
         });
 
         // eslint-disable-next-line no-underscore-dangle
@@ -79,30 +77,36 @@ const Locks = new class extends Mongo.Collection<LockType> {
           return critSection(lock);
         }
 
-        // Lock is held, so wait until we can preempt and try again
-        const monitorTimeout = () => {
-          const otherLock = cursor.fetch()[0];
-          if (!otherLock) {
-            // We raced with the cursor notification
-            cleanupWatches();
-            removed.return(undefined);
-            return;
-          }
+        // Lock is held, so wait until we can preempt
+        const timedOut = new Promise<LockType | undefined>((resolve) => {
+          const waitForDeadline = () => {
+            const otherLock = cursor.fetch()[0];
+            if (!otherLock) {
+              // Lock was deleted, so removed promise will resolve
+              resolve(undefined);
+              return;
+            }
 
-          const time = otherLock.renewedAt || otherLock.createdAt;
-          const timeout = (time.getTime() + PREEMPT_TIMEOUT) - (new Date()).getTime();
+            const deadline =
+              (otherLock.renewedAt || otherLock.createdAt).getTime() +
+              PREEMPT_TIMEOUT;
+            const timeout = deadline - Date.now();
 
-          if (timeout < 0) {
-            cleanupWatches();
-            removed.return(otherLock);
-          } else {
-            timeoutHandle = Meteor.setTimeout(() => monitorTimeout(), timeout);
-          }
-        };
-        monitorTimeout();
+            if (timeout <= 0) {
+              // Lock is expired, so we can preempt it
+              resolve(otherLock);
+              return;
+            }
+
+            // Otherwise wait until expiration and then check again
+            timeoutHandle = Meteor.setTimeout(waitForDeadline, timeout);
+          };
+          waitForDeadline();
+        });
 
         // If we time out, then preempt
-        const preemptableLock = removed.wait();
+        const preemptableLock = MeteorPromise.await(Promise.race([removed, timedOut]));
+        cleanupWatches();
         if (preemptableLock) {
           Ansible.log('Preempting lock', { id: preemptableLock._id, name });
           this.remove({ _id: preemptableLock._id, renewedAt: preemptableLock.renewedAt });
