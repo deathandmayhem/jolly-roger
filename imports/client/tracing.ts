5aeeefc0bd1146c33d42ef31b2d596ae7ab046e7
Drew Fisher <drew.m.fisher@gmail.com>
2022-01-13 02:36:04 -0800

    Add some simple trace recording tools
    My primary goal here was to have something I could turn on for myself at
    runtime from a JS console in production without causing notable
    performance degradation or increased memory usage for anyone else.
    
    So here's some minimal functions that we can get at from the console,
    with some helpers for recording/replaying/dumping traces.
    
    This has the potential to cause nontrivial memory usage when tracing is
    enabled, as I didn't bother dropping traces or capping the trace buffer
    size or anything.  I recommend leaving it off unless you're actively
    debugging an issue in prod that you're unable to reproduce locally,
    which is unfortunately where I am right now with the sidebar scrolling
    on page load.
    
    To enable tracing on initial page load, run
    `localStorage.setItem("tracingEnabled", 1)` from the browser JS console.
    
    To disable tracing on page load, run
    `localStorage.removeItem("tracingEnabled")` from the browser JS console.
    
    The `Tracing` module can be accessed from a global in the browser JS
    console after running `await loadFacades()`.  Of particular use for
    offline examination is `Tracing.dumpBuffer()`, which will generate a
    string which can usually be copied to the clipboard for use elsewhere.

---
+++ imports/client/tracing.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+
+// A dumb, mostly-oblivious trace buffer to enable getting some event ordering
+// information out of production, hopefully  without generating terrible
+// overhead or memory usage unless manually enabled by a user.
+
+// Allow initial trace state to be set via localStorage, so we can
+// enable recording traces on initial page load before a human can
+// poke things in the console.
+let TRACING_ENABLED = localStorage.getItem('enableTracing') !== null;
+
+type TraceItem = {
+  stamp: number;
+  args: any;
+}
+
+const eventBuffer: TraceItem[] = [];
+const trace = (...args: any[]) => {
+  if (TRACING_ENABLED) {
+    const now = performance.now();
+    eventBuffer.push({
+      stamp: now,
+      args,
+    });
+    console.log(now, ...args);
+  }
+};
+
+const replayEvents = () => {
+  eventBuffer.forEach(({ stamp, args }) => {
+    console.log(stamp, ...args);
+  });
+};
+
+const begin = () => {
+  TRACING_ENABLED = true;
+};
+
+const end = () => {
+  TRACING_ENABLED = false;
+};
+
+const clear = () => {
+  eventBuffer.splice(0, eventBuffer.length);
+};
+
+const dumpBuffer = () => {
+  console.log(JSON.stringify(eventBuffer));
+};
+
+export {
+  dumpBuffer, eventBuffer, replayEvents, begin, end, clear, trace,
+};
