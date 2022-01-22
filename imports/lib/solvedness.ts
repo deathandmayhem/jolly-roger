ff94bee56608e0eabac74759d3acf41efbfa6ef2
Drew Fisher <drew.m.fisher@gmail.com>
2022-01-21 16:25:51 -0800

    Whoops, got the test backward

--- imports/lib/solvedness.ts
+++ imports/lib/solvedness.ts
@@ -4,7 +4,7 @@ export type Solvedness = 'noAnswers' | 'solved' | 'unsolved';
 export const computeSolvedness = (puzzle: PuzzleType): Solvedness => {
   if (puzzle.expectedAnswerCount === 0) {
     return 'noAnswers';
-  } else if (puzzle.expectedAnswerCount < puzzle.answers.length) {
+  } else if (puzzle.answers.length < puzzle.expectedAnswerCount) {
     return 'unsolved';
   } else {
     return 'solved';
