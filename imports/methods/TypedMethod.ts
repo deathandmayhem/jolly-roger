3b7cf4c95bd794289bd45b3df0f13903543c246a
Evan Broder <evan@ebroder.net>
2022-07-28 16:32:13 -0700

    Support void args + return and async TypedMethods
    Three major changes here:
    
    - Meteor has supported methods that return a promise for ages, so it's
      easy (and useful) for us to add support for that as well.
    - Specialize the definition of callback functions for methods that
      return void to make clear that there will never be a result argument.
    - Finally, support methods with void arguments. This is, frankly, a bit
      messy but comes up frequently enough to be worth doing. Methods with
      void arguments do not implement a validate method at all, and we use
      this to bridge as best we can between the type system and the runtime
      system. (See the comment around `TypedMethod.define` for more.)

--- imports/methods/TypedMethod.ts
+++ imports/methods/TypedMethod.ts
@@ -2,13 +2,32 @@ import { check } from 'meteor/check';
 import { EJSONable, EJSONableProperty } from 'meteor/ejson';
 import { Meteor } from 'meteor/meteor';
 
-type TypedMethodParam = EJSONable | EJSONableProperty
-type TypedMethodArgs = Record<string, TypedMethodParam>;
+type TypedMethodParam = EJSONable | EJSONableProperty;
+type TypedMethodArgs = Record<string, TypedMethodParam> | void;
 type TypedMethodValidator<Arg extends TypedMethodArgs> =
   (this: Meteor.MethodThisType, arg0: unknown) =>
     Arg;
 type TypedMethodRun<Arg extends TypedMethodArgs, Return extends TypedMethodParam | void> =
-  (this: Meteor.MethodThisType, arg0: Arg) => Return;
+  Arg extends void ?
+  (this: Meteor.MethodThisType) => Return | Promise<Return> :
+  (this: Meteor.MethodThisType, arg0: Arg) => Return | Promise<Return>;
+type TypedMethodCallback<Return extends TypedMethodParam | void> =
+  Return extends void ?
+    (error?: Meteor.Error) => void :
+    <Error extends true | false>(
+      error: Error extends true ? Meteor.Error : undefined,
+      result: Error extends false ? Return : undefined,
+    ) => void;
+type TypedMethodCallArgs<Arg extends TypedMethodArgs, Return extends TypedMethodParam | void> =
+  Arg extends void ?
+    ([TypedMethodCallback<Return>] | []) :
+    ([Arg, TypedMethodCallback<Return>] | [Arg]);
+type TypedMethodCallPromiseArgs<Arg extends TypedMethodArgs> =
+  Arg extends void ?
+    [] :
+    [Arg];
+
+const voidValidator = () => { /* noop */ };
 
 export default class TypedMethod<
   Args extends TypedMethodArgs,
@@ -27,15 +46,32 @@ export default class TypedMethod<
     this.name = name;
   }
 
+  // Supporting methods with no (void) arguments is a bit messy, but comes up
+  // often enough that it's worth doing properly. With no arguments, the type
+  // system doesn't accept a validator. However, there's no way to access type
+  // information at runtime, so when no validator is provided, we substitute
+  // `voidValidator` at runtime (which explicitly discards any arguments,
+  // ensuring that they aren't passed through to the `run` implementation).
+  //
+  // Ideally we'd declare the `validate` method in a way that it is absent for
+  // void and present for non-void and subsequently check for the
+  // presence/absence to hint to the type system whether or not we were dealing
+  // with a void method, but I wasn't able to get the type system to make
+  // inference in that direction (that the absence of a validate method implied
+  // void arguments), which just made everything more awkward.
   define({ validate, run }: {
-    validate: TypedMethodValidator<Args>,
     run: TypedMethodRun<Args, Return>,
-  }) {
+  } & (
+    Args extends void ? { validate?: undefined } : { validate: TypedMethodValidator<Args> }
+  )) {
     if (this.definition) {
       throw new Error(`TypedMethod ${this.name} has already been defined`);
     }
 
-    this.definition = { validate, run };
+    this.definition = {
+      validate: (validate || voidValidator) as TypedMethodValidator<Args>,
+      run,
+    };
 
     /* eslint-disable-next-line @typescript-eslint/no-this-alias -- we need to
        capture both the TypedMethod object (so we can find the implementation)
@@ -47,29 +83,29 @@ export default class TypedMethod<
         but the actual Meteor audit-argument-checks package will still fire if
         we don't use check methods. */
       [this.name](arg0: Args) {
-        self.execute(this, arg0);
+        return self.execute(this, arg0);
       },
     });
   }
 
-  execute(context: Meteor.MethodThisType, arg0: Args): Return {
+  execute(context: Meteor.MethodThisType, arg0: Args): Return | Promise<Return> {
     if (!this.definition) {
       throw new Error(`TypedMethod ${this.name} has not been defined`);
     }
 
+    // In the case of no arguments, the type system will track the return value
+    // from `validate` as `void`, but because it's a function that doesn't
+    // return anything, it will in practice be `undefined`.
     const validatedArgs = this.definition.validate.bind(context)(arg0);
-    const result = this.definition.run.bind(context)(validatedArgs);
+    const result = this.definition.run.bind(context)(validatedArgs as any);
     return result;
   }
 
-  call(
-    arg0: Args,
-    callback?: (error: Meteor.Error, result: Return) => void,
-  ): void {
-    return Meteor.call(this.name, arg0, callback);
+  call(...args: TypedMethodCallArgs<Args, Return>): void {
+    return Meteor.call(this.name, ...args);
   }
 
-  callPromise(arg0: Args): Promise<Return> {
-    return Meteor.callPromise(this.name, arg0);
+  callPromise(...args: TypedMethodCallPromiseArgs<Args>): Promise<Return> {
+    return Meteor.callPromise(this.name, ...args);
   }
 }
