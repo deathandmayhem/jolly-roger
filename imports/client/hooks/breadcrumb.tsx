541b3e312bfb0e85ccebbaa2ba348c25ff30310a
Drew Fisher <drew.m.fisher@gmail.com>
2022-08-16 00:38:24 -0700

    Convert breadcrumb hooks to run purely in effects
    The previous code triggered side-effects during render, to attempt to
    register breadcrumb IDs in component-hierarchical order.  However, this
    approach was unsound, and React 18's StrictMode revealed significant
    issues with this approach.
    
    Instead, we now take a simpler approach, wherein we stick to only
    registering breadcrumbs in effect blocks.  Because we cannot make
    guarantees about components mounting and updating (in particular,
    renaming a hunt was an easy way to trigger breadcrumb reordering), we no
    longer rely on hierarchical order, and instead order breadcrumbs by
    sorting by path (href) length ascending, which increases as URLs become
    increasingly detailed.

--- imports/client/hooks/breadcrumb.tsx
+++ imports/client/hooks/breadcrumb.tsx
@@ -3,7 +3,6 @@ import { Random } from 'meteor/random';
 import React, {
   useCallback, useContext, useEffect, useMemo, useRef, useState,
 } from 'react';
-import useImmediateEffect from './useImmediateEffect';
 
 type Crumb = {
   path: string;
@@ -23,18 +22,16 @@ type BreadcrumbSubscribeHandle = {
 type BreadcrumbSubscribeCallback = (crumbs: CrumbWithId[]) => void;
 
 type BreadcrumbContextType = {
-  addCrumb: (crumb: Crumb) => CrumbId;
+  addCrumb: (path: string, title: string) => CrumbId;
   removeCrumb: (crumbId: CrumbId) => void;
-  updateCrumb: (crumbId: CrumbId, crumb: Crumb) => void;
-  flushUpdates: () => void;
+  updateCrumb: (crumbId: CrumbId, path: string, title: string) => void;
   subscribe: (listener: BreadcrumbSubscribeCallback) => BreadcrumbSubscribeHandle;
 }
 
 const defaultCallbacks: BreadcrumbContextType = {
-  addCrumb: (_crumb: Crumb) => { return Random.id(); },
+  addCrumb: (_path: string, _title: string) => { return Random.id(); },
   removeCrumb: (_crumbId: CrumbId) => { /* noop */ },
-  updateCrumb: (_crumbId: CrumbId, _crumb: Crumb) => { /* noop */ },
-  flushUpdates: () => { /* noop */ },
+  updateCrumb: (_crumbId: CrumbId, _path: string, _title: string) => { /* noop */ },
   subscribe: (_listener: BreadcrumbSubscribeCallback) => {
     return {
       unsubscribe() {
@@ -46,32 +43,26 @@ const defaultCallbacks: BreadcrumbContextType = {
 
 const BreadcrumbContext = React.createContext<BreadcrumbContextType>(defaultCallbacks);
 
+const byPathLength = (c1: CrumbWithId, c2: CrumbWithId) => {
+  return c1.path.length - c2.path.length;
+};
+
 const BreadcrumbsProvider = ({ children }: { children: React.ReactNode }) => {
   const crumbsRef = useRef<CrumbWithId[]>([]);
   const listenersRef = useRef<((crumbs: CrumbWithId[]) => void)[]>([]);
 
-  const flushUpdates = useCallback(() => {
-    listenersRef.current.forEach((listener) => listener(crumbsRef.current));
-  }, []);
-
-  const addCrumb = useCallback((crumb: Crumb) => {
+  const addCrumb = useCallback((path: string, title: string) => {
     // Generate a new crumb ID, as this is a new crumb
     const crumbId = Random.id();
     const crumbWithId = {
       id: crumbId,
-      title: crumb.title,
-      path: crumb.path,
+      path,
+      title,
     };
-    // console.log(`added crumb ${crumbId} title: ${crumb.title} path: ${crumb.path}`);
+    // console.log(`added crumb ${crumbId} title: ${title} path: ${path}`);
     crumbsRef.current = [...crumbsRef.current, crumbWithId];
-
-    // We need to defer the breadcrumb update flushes until we're no longer
-    // rendering other components, since `addCrumb` gets called on initial
-    // render.  `useBreadcrumb` calls `addCrumb` immediately, and triggers a
-    // flush to run during effect time.  So we don't dispatch the listeners,
-    // and instead rely on the caller to call `flushUpdates()` later, once we're
-    // into the effects phase.
-
+    crumbsRef.current.sort(byPathLength);
+    listenersRef.current.forEach((listener) => listener(crumbsRef.current));
     return crumbId;
   }, []);
 
@@ -87,25 +78,26 @@ const BreadcrumbsProvider = ({ children }: { children: React.ReactNode }) => {
     const beforeRemoved = prevCrumbs.slice(0, crumbIndex);
     const afterRemoved = prevCrumbs.slice(crumbIndex + 1, prevCrumbs.length);
     crumbsRef.current = beforeRemoved.concat(afterRemoved);
-    // removeCrumb runs in the useEffect cleanup phase, so it's safe to dispatch listeners
+    crumbsRef.current.sort(byPathLength);
     listenersRef.current.forEach((listener) => listener(crumbsRef.current));
   }, []);
 
-  const updateCrumb = useCallback((crumbId: CrumbId, crumb: Crumb) => {
-    // console.log(`updating crumb ${crumbId} to title: ${crumb.title} path: ${crumb.path}`);
+  const updateCrumb = useCallback((crumbId: CrumbId, path: string, title: string) => {
+    // console.log(`updating crumb ${crumbId} to title: ${title} path: ${path}`);
     const prevCrumbs = crumbsRef.current;
     const crumbIndex = prevCrumbs.findIndex((c) => c.id === crumbId);
     const newCrumbWithId = {
       id: crumbId,
-      title: crumb.title,
-      path: crumb.path,
+      path,
+      title,
     };
 
     const beforeUpdated = prevCrumbs.slice(0, crumbIndex);
     const afterUpdated = prevCrumbs.slice(crumbIndex + 1, prevCrumbs.length);
     crumbsRef.current = beforeUpdated.concat([newCrumbWithId]).concat(afterUpdated);
+    crumbsRef.current.sort(byPathLength);
 
-    // updateCrumb is run from an effect phase, so we can immediately dispatch listeners.
+    // console.log(`updating ${listenersRef.current.length} subscribers`);
     listenersRef.current.forEach((listener) => listener(crumbsRef.current));
   }, []);
 
@@ -125,9 +117,8 @@ const BreadcrumbsProvider = ({ children }: { children: React.ReactNode }) => {
     addCrumb,
     removeCrumb,
     updateCrumb,
-    flushUpdates,
     subscribe,
-  }), [addCrumb, removeCrumb, updateCrumb, flushUpdates, subscribe]);
+  }), [addCrumb, removeCrumb, updateCrumb, subscribe]);
 
   return (
     <BreadcrumbContext.Provider value={providerCallbacks}>
@@ -137,60 +128,46 @@ const BreadcrumbsProvider = ({ children }: { children: React.ReactNode }) => {
 };
 
 function useBreadcrumb(crumb: Crumb): void {
+  // console.log("useBreadcrumb(", crumb, ")");
   const ctx = useContext<BreadcrumbContextType>(BreadcrumbContext);
   const crumbId = useRef<string | null>(null);
-  const firstRender = useRef<boolean>(true);
 
   const {
-    addCrumb, removeCrumb, updateCrumb, flushUpdates,
+    addCrumb, removeCrumb, updateCrumb,
   } = ctx;
 
-  useImmediateEffect(() => {
-    crumbId.current = addCrumb(crumb);
-    // console.log(`mount ${crumbId.current}`);
+  useEffect(() => {
+    if (crumbId.current === null) {
+      crumbId.current = addCrumb(crumb.path, crumb.title);
+    } else {
+      updateCrumb(crumbId.current, crumb.path, crumb.title);
+    }
+  }, [addCrumb, updateCrumb, crumb.path, crumb.title]);
 
+  useEffect(() => {
     return () => {
       if (crumbId.current) {
-        // console.log(`unmount ${crumbId.current}`);
         removeCrumb(crumbId.current);
         crumbId.current = null;
       }
     };
-  }, []);
-
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      // On first render, trigger a flush of the crumbs, since `addCrumb` has
-      // to run immediately (to preserve breadcrumb mount order), but the
-      // setState it will trigger can't run during the render phase.
-      flushUpdates();
-    }
-  }, [flushUpdates]);
-
-  useEffect(() => {
-    if (crumbId.current) {
-      // console.log(`update ${crumbId.current}`);
-      updateCrumb(crumbId.current, crumb);
-    }
-  }, [updateCrumb, crumb]);
+  }, [removeCrumb]);
 }
 
 const useBreadcrumbItems = () => {
   const subscriptionRef = useRef<BreadcrumbSubscribeHandle | undefined>(undefined);
   const [crumbs, setCrumbs] = useState<CrumbWithId[]>([]);
   const ctx = useContext(BreadcrumbContext);
-  useImmediateEffect(() => {
-    subscriptionRef.current = ctx.subscribe(setCrumbs);
-  }, []);
+  const { subscribe } = ctx;
 
   useEffect(() => {
+    subscriptionRef.current = subscribe(setCrumbs);
     return () => {
       if (subscriptionRef.current) {
         subscriptionRef.current.unsubscribe();
       }
     };
-  }, []);
+  }, [subscribe]);
 
   return crumbs;
 };
