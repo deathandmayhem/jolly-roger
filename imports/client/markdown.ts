425af38a9dc0a7a255e5fbdfeabf7b23f1e07380
Evan Broder <evan@ebroder.net>
2022-11-25 12:55:36 -0800

    Upgrade to modern version of marked
    Fixes #1162 (and as a side-effect, changes Markdown to standard
    formatting).

--- imports/client/markdown.ts
+++ imports/client/markdown.ts
@@ -1,13 +1,9 @@
 import DOMPurify from 'dompurify';
-import marked from 'marked';
+import { marked, Renderer } from 'marked';
 
-(marked.InlineLexer.rules.gfm as marked.Rules).em = /^\b_((?:__|[^_])+?)_\b/;
-(marked.InlineLexer.rules.gfm as marked.Rules).strong = /^\*\b((?:\*\*|[^*])+?)\b\*/;
-
-const renderer = new class extends marked.Renderer {
+const renderer = new class extends Renderer {
   link(href: string, title: string, link: string) {
-    const realLinkFunc = marked.Renderer.prototype.link.bind(this);
-    return realLinkFunc(href, title, link)
+    return super.link(href, title, link)
       .replace(/^<a /, '<a target="_blank" rel="noopener noreferrer" ');
   }
 }();
