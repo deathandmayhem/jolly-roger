dc423e5bc55568fa7959deeea46d38215ba3a2a4
Drew Fisher <drew.m.fisher@gmail.com>
2022-11-25 02:36:34 -0800

    Replace `meteorhacks:cluster` process load balancer
    This follows the same overall architecture (same dependencies, same
    general flow), but drops large chunks of functionality we did not use,
    and is basically completely rewritten with ES6 classes and TypeScript.
    
    Incidentally, as `meteorhacks:cluster` was the last remaining
    client-side importer of `underscore`, this change has the side effect of
    reducing the bundle size from 1.77 MB to 1.75 MB.

---
+++ imports/server/LoadBalancer.ts
@@ -0,0 +1,173 @@
+/* eslint-disable no-underscore-dangle */
+import { IncomingMessage, ServerResponse } from 'http';
+import stream from 'stream';
+import { WebApp } from 'meteor/webapp';
+import HttpProxy from 'http-proxy';
+import WorkerPool, { Worker } from './WorkerPool';
+
+// This implements a simple multi-process, single-machine load-balancer.
+//
+// It serves as a drop-in replacement for the relevant subset of the
+// `meteorhacks:cluster` package we used (which was mostly just "load balance
+// across a pool of processes, so we can use all the cores on a machine").
+//
+// The coordinator runs in the main process, and may spawn several worker
+// processes (which will have behavior modulated by having CLUSTER_WORKER_ID set
+// in their environment).
+//
+// The coordinator intercepts HTTP and Websocket requests, and may dispatch them
+// to worker processes over `child_process` messaging.
+//
+// If CLUSTER_WORKERS_COUNT is set in the environment, it spawns that many child
+// processes, or if set to "auto", spawns as many children as the machine has
+// CPUs.
+//
+// Otherwise, it spawns no children, and the listeners simply propagate the
+// requests as middleware usually do, which causes the requests to be serviced
+// from the main process.
+export default class LoadBalancer {
+  proxy: HttpProxy;
+
+  workers: WorkerPool;
+
+  workerMapping: Record<string, { worker: Worker; lastUpdate: number; }>;
+
+  constructor(proxy: HttpProxy, workers: WorkerPool) {
+    this.proxy = proxy;
+    this.workers = workers;
+    this.workerMapping = {};
+  }
+
+  httpHandler(): (req: IncomingMessage, res: ServerResponse) => boolean {
+    const proxy = this.proxy;
+    const workers = this.workers;
+    const workerMapping = this.workerMapping;
+    const handleHttp = function (req: IncomingMessage, res: ServerResponse): boolean {
+      // Only intercept if we're the coordinator process.  If we have a worker ID,
+      // we're not the coordinator.
+      if (process.env.CLUSTER_WORKER_ID) {
+        return false;
+      }
+
+      // Only intercept sockjs HTTP requests.
+      const longPollingMatcher = /^\/sockjs\/([0-9]+)\/(\w+)\/xhr/;
+      const match = req.url?.match(longPollingMatcher);
+      if (match) {
+        // We are the coordinator.  Select a worker and dispatch the request.
+        // We want to have the same backend servicing requests from the same client, so
+        // we aim to pick a consistent worker for a given sockjs connection string.
+        const id = `${match[1]}${match[2]}`;
+        if (!workerMapping[id]) {
+          const worker = workers && workers.pickWorker();
+          if (worker) {
+            workerMapping[id] = { worker, lastUpdate: Date.now() };
+          }
+        }
+
+        if (workerMapping[id]) {
+          workerMapping[id].lastUpdate = Date.now();
+          const target = {
+            host: '127.0.0.1',
+            port: workerMapping[id].worker.port,
+          };
+          // Set long timeout because clients long-poll
+          res.setTimeout(2 * 60 * 1000);
+          // Proxy the request.  On failure, clean up the worker mapping, so we try
+          // a different worker.
+          proxy.web(req, res, { target }, () => {
+            delete workerMapping[id];
+          });
+          return true;
+        } else {
+          // Continue middleware propagation if we could not get a worker.
+          return false;
+        }
+      } else {
+        // Do not send other HTTP requests to workers.
+        return false;
+      }
+    };
+    return handleHttp;
+  }
+
+  wsHandler(): (req: IncomingMessage, socket: any, head: Buffer) => boolean {
+    const workers = this.workers;
+    const handleWs = function (req: IncomingMessage, socket: any, head: Buffer): boolean {
+      // Only intercept if we're the coordinator process.  If we have a worker ID,
+      // we're not the coordinator.
+      if (process.env.CLUSTER_WORKER_ID) {
+        return false;
+      }
+
+      const worker = workers?.pickWorker();
+      // If we couldn't find a usable worker, just service the request from the main
+      // process
+      if (!worker) return false;
+
+      // Otherwise, dispatch the request to the worker
+      worker.process.send({
+        type: 'proxy-ws',
+        req: {
+          readable: req.readable,
+          domain: (req as any).domain,
+          httpVersion: req.httpVersion,
+          complete: req.complete,
+          headers: req.headers,
+          trailers: req.trailers,
+          _pendings: (req as any)._pendings,
+          _pendingIndex: (req as any)._pendingIndex,
+          url: req.url,
+          method: req.method,
+          statusCode: req.statusCode,
+          _consuming: (req as any)._consuming,
+          _dumped: (req as any)._dumped,
+          httpVersionMajor: req.httpVersionMajor,
+          httpVersionMinor: req.httpVersionMinor,
+          upgrade: (req as any).upgrade,
+        },
+        head: head.toString('utf8'),
+      }, socket);
+      return true;
+    };
+    return handleWs;
+  }
+
+  installHttp() {
+    const event = 'request';
+    const httpServer = WebApp.httpServer;
+    const oldHttpServerListeners = httpServer.listeners(event).slice(0);
+    httpServer.removeAllListeners(event);
+
+    const httpHandler = this.httpHandler();
+    const newListener = function (req: IncomingMessage, res: ServerResponse) {
+      if (httpHandler.apply(httpServer, [req, res]) !== true) {
+        oldHttpServerListeners.forEach((oldListener) => {
+          oldListener.apply(httpServer, [req, res]);
+        });
+      }
+    };
+    httpServer.addListener(event, newListener);
+  }
+
+  installWs() {
+    const event = 'upgrade';
+    const httpServer = WebApp.httpServer;
+    const oldHttpServerListeners = httpServer.listeners(event).slice(0);
+    httpServer.removeAllListeners(event);
+
+    const wsHandler = this.wsHandler();
+    const newListener = function (req: IncomingMessage, socket: stream.Duplex, head: Buffer) {
+      if (wsHandler.apply(httpServer, [req, socket, head]) !== true) {
+        oldHttpServerListeners.forEach((oldListener) => {
+          oldListener.apply(httpServer, [req, socket, head]);
+        });
+      }
+    };
+    httpServer.addListener(event, newListener);
+  }
+
+  install() {
+    this.installHttp();
+    this.installWs();
+  }
+}
---
+++ imports/server/WorkerPool.ts
@@ -0,0 +1,169 @@
+/* eslint-disable camelcase */
+import child_process from 'child_process';
+import { WebApp } from 'meteor/webapp';
+import portscanner from 'portscanner';
+
+type HandledSignal = 'SIGINT' | 'SIGHUP' | 'SIGTERM';
+
+export type Worker = {
+  process: child_process.ChildProcess;
+  id: number;
+  port: number;
+}
+
+// worker processes should notify their parent once they're ready to accept
+// requests
+if (process.env.CLUSTER_WORKER_ID) {
+  WebApp.onListening(() => {
+    if (process.send) {
+      process.send({
+        type: 'ready',
+      });
+    }
+  });
+}
+
+export default class WorkerPool {
+  exec: string;
+
+  args: string[];
+
+  workers: Worker[];
+
+  workersMap: Record<number, Worker>;
+
+  ids: number;
+
+  closed: boolean;
+
+  recentReconnects: number;
+
+  lastReconnectAt: number;
+
+  static create(size: number): WorkerPool | undefined {
+    if (process.env.CLUSTER_WORKER_ID) {
+      return undefined;
+    }
+    return new WorkerPool(size);
+  }
+
+  constructor(size: number) {
+    this.exec = process.argv[1];
+    this.args = process.argv.slice(2);
+    this.workers = [];
+    this.workersMap = {};
+    this.ids = 0;
+    this.closed = false;
+    for (let i = 0; i < size; i++) {
+      this.createWorker();
+    }
+    this.recentReconnects = 0;
+    this.lastReconnectAt = 0;
+
+    (['SIGINT', 'SIGHUP', 'SIGTERM'] as const).forEach((signal: HandledSignal) => {
+      process.once(signal, this.cleanup.bind(this));
+    });
+  }
+
+  createWorker() {
+    this.fork((worker) => {
+      const logMessage = `Multiprocess: worker ${worker.id} starting on port ${worker.port}`;
+      // eslint-disable-next-line no-console
+      console.info(logMessage);
+
+      const registerWorker = (message: any) => {
+        if (message && message.type === 'ready') {
+          this.workers.push(worker);
+          this.workersMap[worker.id] = worker;
+
+          process.removeListener('message', registerWorker);
+        }
+      };
+
+      worker.process.on('message', registerWorker);
+      worker.process.once('exit', (exitCode, signalCode) => {
+        const exitMessage = `Multiprocess: worker ${worker.id} exiting with exitCode=${exitCode} signalCode=${signalCode}`;
+        // eslint-disable-next-line no-console
+        console.info(exitMessage);
+
+        const index = this.workers.indexOf(worker);
+        if (index >= 0) {
+          this.workers.splice(index, 1);
+          delete this.workersMap[worker.id];
+        }
+        if (!this.closed) {
+          const reconnectTimeout = this.getReconnectTimeout();
+          if (reconnectTimeout === 0) {
+            this.createWorker();
+          } else {
+            setTimeout(this.createWorker.bind(this), reconnectTimeout);
+          }
+        }
+      });
+    });
+  }
+
+  getReconnectTimeout(): number {
+    const now = Date.now();
+    const timeDiff = now - this.lastReconnectAt;
+    const oneMinute = 1000 * 60;
+    if (timeDiff > oneMinute) {
+      this.recentReconnects = 0;
+    }
+
+    const reconnectTime = this.recentReconnects * 500;
+    this.recentReconnects += 1;
+    this.lastReconnectAt = now;
+    return reconnectTime;
+  }
+
+  fork(callback: (w: Worker) => void) {
+    const id = this.ids;
+    this.ids += 1;
+
+    const firstPort = Math.ceil(Math.random() * 20000) + 2000;
+    const secondPort = firstPort + 1;
+
+    const withPort = (error: Error | null, port: number) => {
+      if (error) throw error;
+      const env = {
+        ...process.env,
+        PORT: `${port}`,
+        CLUSTER_WORKER_ID: `${id}`,
+      };
+      const child = child_process.fork(this.exec, this.args, {
+        env,
+        silent: false,
+      });
+      const worker = {
+        process: child,
+        id,
+        port,
+      };
+
+      callback(worker);
+    };
+
+    portscanner.findAPortNotInUse(firstPort, secondPort, '127.0.0.1', withPort);
+  }
+
+  cleanup(signal: NodeJS.Signals) {
+    this.closed = true;
+    this.workers.forEach((worker) => {
+      worker.process.kill(signal);
+    });
+    process.kill(process.pid, signal);
+  }
+
+  pickWorker(): Worker | undefined {
+    const workerCount = this.workers.length;
+    if (!workerCount) return undefined;
+    const index = Math.floor(workerCount * Math.random());
+    const worker = this.workers[index];
+    return {
+      id: worker.id,
+      port: worker.port,
+      process: worker.process,
+    };
+  }
+}
