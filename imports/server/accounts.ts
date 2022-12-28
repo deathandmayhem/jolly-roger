da2520e2f6ab7c5d16e3202c589368f31f4801a2
Evan Broder <evan@ebroder.net>
2022-12-28 13:23:34 -0800

    Make some improvements to lint config
    - Enable a handful of new rules from typescript-eslint's strict set.
      This includes "no-non-null-asserted-nullish-coalescing",
      "non-nullable-type-assertion-style", "prefer-for-of",
      "prefer-return-this-type", and "prefer-string-starts-ends-with". These
      are mostly stylistic improvements and require very few changes to
      existing code, although "non-nullable-type-assertion-style" did catch
      an unintentional and unnecessary casting away of nullability.
    - Pull all directives into the top-level .eslintrc.json. Previously, we
      had some environment declarations in the server and client
      directories, but we can just put those in top-level overrides. This is
      both more correct (as it now handles any directory with "client" or
      "server", not just the ones directly under imports/) and more
      discoverable.
    - Be consistent about style - if we are only turning a rule on or off,
      we don't need to pass the new level as an array; we can just pass it
      directly as a string.
    - Disable any rules which have been tripping in our ambient type
      declarations under types/ - we don't come up with these declarations,
      so any error we run into is a linting failure, not a code failure.

--- imports/server/accounts.ts
+++ imports/server/accounts.ts
@@ -115,9 +115,9 @@ const DEFAULT_ENROLL_ACCOUNT_TEMPLATE = 'Hiya!\n' +
     '\n' +
     'This message was sent to {{email}}';
 
-function makeView(user: Meteor.User | null, url: string) {
+function makeView(user: Meteor.User, url: string) {
   // eslint-disable-next-line jolly-roger/no-sync-mongo-methods
-  const hunts = Hunts.find({ _id: { $in: (<Meteor.User>user).hunts } }).fetch();
+  const hunts = Hunts.find({ _id: { $in: user.hunts } }).fetch();
   const email = user?.emails?.[0]?.address;
   const huntNames = hunts.map((h) => h.name);
   const huntNamesCount = huntNames.length;
