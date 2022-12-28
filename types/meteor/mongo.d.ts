12f9690757c23d5cfd10b6a368b03038ad7b512b
Evan Broder <evan@ebroder.net>
2022-12-27 20:33:58 -0800

    Allow for-of loop syntax
    The Airbnb style guide forbids using for-of loops. It warns that the
    runtime overhead required for generators is high, and encourages using
    forEach methods on arrays instead. However, in cases where we are
    iterating over results from a database query (e.g. where we previously
    would have used forEach), an async for-of loop is the only way to
    iterate without first fetching the entire result set into memory. And
    Meteor enables the regenerator runtime by default, so that's not
    relevant either.
    
    This enables for-of loops (and also makes a small correction to Meteor's
    type declarations to allow for-of loops to work).

---
+++ types/meteor/mongo.d.ts
@@ -0,0 +1,11 @@
+import 'meteor/mongo';
+
+declare module 'meteor/mongo' {
+  namespace Mongo {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Cursor<T, U> {
+      [Symbol.iterator](): Iterator<T>;
+      [Symbol.asyncIterator](): AsyncIterator<T>;
+    }
+  }
+}
