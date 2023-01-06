ea1202e6bd1a67e614c3bc40ef8acf4f1998ab19
Evan Broder <evan@ebroder.net>
2023-01-06 10:45:23 -0800

    Fix server-side source code capture for Bugsnag (Take 2)
    If you supply a projectRoot, Bugsnag will find source code in that
    directory and include it as part of the error report. This doesn't work
    out of the box in production because we don't have the non-compiled
    source anymore. However, we do have it in the form of sourcemaps (which
    contain the full uncompiled source code). So use the source maps to
    materialize a temporary directory to use as a projectRoot for Bugsnag.
    
    (We can't get Bugsnag to use the sourcemaps because Meteor installs
    source-map-support, which rewrites the stack traces before Bugsnag gets
    a chance to see them, so Bugsnag can't map the post-rewrite stack frame
    onto the source map, which, since it refers to the post-compilation
    filename, doesn't match)
    
    Unlike the first attempt in 7107fb74, this drops the source-map
    repository, which uses wasm for faster parsing of mappings data. That
    isn't compatible with Meteor's use of fibers, but we don't need it, so
    it doesn't matter.

--- imports/server/bugsnag.ts
+++ imports/server/bugsnag.ts
@@ -1,16 +1,107 @@
+import { promises as fs } from 'fs';
+import os from 'os';
+import path from 'path';
+import { promisify } from 'util';
+import { fetch } from 'meteor/fetch';
 import { Meteor } from 'meteor/meteor';
 import Bugsnag from '@bugsnag/js';
+import glob from 'glob';
+import Ansible from '../Ansible';
 import isAdmin from '../lib/isAdmin';
 import MeteorUsers from '../lib/models/MeteorUsers';
 import { userIsOperatorForAnyHunt } from '../lib/permission_stubs';
 import addRuntimeConfig from './addRuntimeConfig';
+import onExit from './onExit';
+
+interface SourceMap {
+  sources: string[];
+  sourcesContent: string[];
+}
 
 const apiKey = process.env.BUGSNAG_API_KEY;
 
 if (apiKey) {
+  const releaseStage = Meteor.isDevelopment ? 'development' : 'production';
+
+  // Record this release so that Bugsnag has tracking for when releases are
+  // deployed
+  Meteor.defer(async () => {
+    try {
+      const resp = await fetch('https://build.bugsnag.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          apiKey,
+          appVersion: Meteor.gitCommitHash,
+          sourceControl: {
+            provider: 'github',
+            repository: 'https://github.com/deathandmayhem/jolly-roger',
+            revision: Meteor.gitCommitHash,
+          },
+          releaseStage,
+        }),
+      });
+      if (!resp.ok) {
+        throw new Error(`Error from Bugsnag build API: ${await resp.text()}`);
+      }
+    } catch (error) {
+      Ansible.error('Unable to report release to Bugsnag', { e: error });
+      if (error instanceof Error && Bugsnag.isStarted()) {
+        Bugsnag.notify(error, (event) => {
+          // eslint-disable-next-line no-param-reassign
+          event.severity = 'warning';
+        });
+      }
+    }
+  });
+
+  // If you supply a projectRoot, Bugsnag will attempt to find source code in
+  // that directory and include it with the event report. This doesn't work for
+  // us out of the box because we don't have the sourcecode present. However, we
+  // do still have it in the form of sourcemaps, so we can use the sourcemaps to
+  // materialize the source tree that Bugsnag expects in a temporary directory.
+  //
+  // (Note: we can't ask Bugsnag to use the sourcemaps itself because Meteor
+  // already installs source-map-support, which means that exceptions have
+  // already had source map transformations applied.)
+  let projectRoot: string | undefined;
+  try {
+    projectRoot = await fs.mkdtemp(path.join(os.tmpdir(), 'jolly-roger-'));
+    onExit(async () => {
+      await fs.rm(projectRoot!, { recursive: true });
+    });
+
+    // Our current working directory is programs/server/, which contains the
+    // server-side code bundle
+    const sourceMapPaths = await promisify(glob)('**/*.map', { cwd: process.cwd() });
+    await Promise.all(sourceMapPaths.map(async (sourceMapPath) => {
+      const sourceMap = JSON.parse(await fs.readFile(sourceMapPath, 'utf8')) as SourceMap;
+      await Promise.all(sourceMap.sources.map(async (sourcePath, i) => {
+        if (!sourcePath.startsWith('meteor://💻app/')) {
+          return;
+        }
+        const source = sourceMap.sourcesContent[i];
+        if (!source) {
+          return;
+        }
+
+        const strippedPath = sourcePath.slice('meteor://💻app/'.length);
+        await fs.mkdir(path.join(projectRoot!, path.dirname(strippedPath)), { recursive: true });
+        await fs.writeFile(path.join(projectRoot!, strippedPath), source);
+      }));
+    }));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Unable to create source tree for Bugsnag', error);
+  }
+
   Bugsnag.start({
     apiKey,
     appVersion: Meteor.gitCommitHash,
+    projectRoot,
+    releaseStage,
     redactedKeys: [
       'password',
       'token',
