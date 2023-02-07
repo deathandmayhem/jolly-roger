f0988230c22b625a9399fa01d0a348c203f1ee0b
Evan Broder <evan@ebroder.net>
2023-02-06 16:35:38 -0800

    Add an async version of Flags.active
    ...and make Flags.observeChanges async friendly.

--- imports/Flags.ts
+++ imports/Flags.ts
@@ -1,35 +1,53 @@
 import { check } from 'meteor/check';
+import type { FeatureFlagType } from './lib/models/FeatureFlags';
 import FeatureFlags from './lib/models/FeatureFlags';
+
+function flagIsActive(flag: Pick<FeatureFlagType, 'type'> | undefined) {
+  if (!flag) {
+    return false;
+  }
+
+  switch (flag.type) {
+    case 'on':
+      return true;
+    case 'off':
+      return false;
+    default:
+      return false;
+  }
+}
 
 const Flags = {
   active(name: unknown) {
     check(name, String);
 
     const flag = FeatureFlags.findOne({ name });
-    if (!flag) {
-      return false;
-    }
+    return flagIsActive(flag);
+  },
 
-    switch (flag.type) {
-      case 'on':
-        return true;
-      case 'off':
-        return false;
-      default:
-        return false;
-    }
+  async activeAsync(name: unknown) {
+    check(name, String);
+
+    const flag = await FeatureFlags.findOneAsync({ name });
+    return flagIsActive(flag);
   },
 
   observeChanges(name: unknown, cb: (active: boolean) => void) {
     check(name, String);
     check(cb, Function);
 
-    let state: boolean | undefined;
-    const checkUpdate = () => {
-      const active = Flags.active(name);
-      if (state !== active) {
-        state = active;
-        cb(active);
+    let state: FeatureFlagType | undefined;
+    const checkUpdate = (_id?: string, flag?: Partial<FeatureFlagType>) => {
+      let newState;
+      if (flag) {
+        newState = { ...state ?? {}, ...flag } as FeatureFlagType;
+      } else {
+        newState = undefined;
+      }
+      const newActive = flagIsActive(newState);
+      if (flagIsActive(state) !== newActive) {
+        state = newState;
+        cb(newActive);
       }
     };
     const handle = FeatureFlags.find({ name }).observeChanges({
