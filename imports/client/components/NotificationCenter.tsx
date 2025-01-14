92c44a40fea3e8f82f30ac39cd15472f7cdc591a
James <jimsug@users.noreply.github.com>
2025-01-14 15:03:57 +1100

    limit chat notifications to 30rem total (#117)
    * limit chat notifications to 30rem total
    
    * Put Unsolved back
    
    * Whoops left in merge conflict. Fixed.
    
    * Abstract Toast max-height to SCSS except for answer submissions …
    
    because they have buttons at the bottom, so the actual answer part needs to be even shorter
    
    ---------
    
    Co-authored-by: James <js@sugrono.com>
    Co-authored-by: Sandor Weisz <sandor@mysteryleague.com>

--- imports/client/components/NotificationCenter.tsx
+++ imports/client/components/NotificationCenter.tsx
@@ -307,7 +307,13 @@ const GuessMessage = React.memo(
           )}
         </Toast.Header>
         <Toast.Body>
-          <StyledNotificationRow>
+          <StyledNotificationRow
+            style={{
+              maxHeight: "15rem",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
             <PuzzleAnswer answer={guess.guess} breakable />
           </StyledNotificationRow>
           <StyledNotificationActionBar>
