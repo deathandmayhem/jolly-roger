1923ef89fe5e3cc23c0d8accbd5fae1c9b8db0c4
James <js@sugrono.com>
2025-01-13 21:46:03 -0500

    fix typo, closes #121

--- imports/client/components/GuessQueuePage.tsx
+++ imports/client/components/GuessQueuePage.tsx
@@ -250,10 +250,10 @@ const GuessBlock = React.memo(
     let confidenceLabel;
     let confidenceVariant;
 
-    if (guess?.confidence > 0) {
+    if (guess?.confidence > 50) {
       confidenceLabel = "High";
       confidenceVariant = "success";
-    } else if (guess?.confidence < -5) {
+    } else if (guess?.confidence < 50) {
       confidenceLabel = "Low";
       confidenceVariant = "danger";
     } else {
