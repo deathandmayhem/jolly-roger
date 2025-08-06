f373b6fc5f31e89336b5b87d0546fbc8287e60d0
Drew Fisher <drew.m.fisher@gmail.com>
2025-08-06 03:43:22 -0700

    Fix migrations that needed `for await` on calls that now return futures

--- imports/server/migrations/48-make-puzzle-tags-unique.ts
+++ imports/server/migrations/48-make-puzzle-tags-unique.ts
@@ -5,7 +5,7 @@ Migrations.add({
   version: 48,
   name: "Make puzzle tags unique",
   async up() {
-    for (const puzzle of Puzzles.find()) {
+    for await (const puzzle of Puzzles.find()) {
       const uniqueTags = [...new Set(puzzle.tags)];
       if (uniqueTags.length === puzzle.tags.length) {
         continue;
