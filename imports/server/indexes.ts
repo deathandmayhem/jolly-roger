97aecb81afabc3c48fdbc9e5ace7c074dc99b391
Drew Fisher <drew.m.fisher@gmail.com>
2025-09-07 19:43:08 -0700

    indexes: handle conflicts with existing indexes by dropping those first

--- imports/server/indexes.ts
+++ imports/server/indexes.ts
@@ -57,33 +57,73 @@ runIfLatestBuild(async () => {
         };
       });
 
-    const existingIndexSet = new Set(
-      existingIndexes.map(({ stringified }) => stringified),
+    // We wish to compute:
+    // * the set of desired indexes we need to add
+    // * the set of existing indexes we need to drop, which is comprised of:
+    //   * the set of existing indexes with names or definitions that collide with the set of desired new indexes, which need to be dropped before creation
+    //   * the set of extra indexes that do not share a name or structure with a desired index, which we prefer to drop after doing new index creation
+    const existingIndexesByName = new Map(
+      existingIndexes.map((idx) => [idx.name, idx]),
     );
-    const expectedIndexSet = new Set(
-      expectedIndexes.map(({ stringified }) => stringified),
+    const existingIndexesByStructure = new Map(
+      existingIndexes.map((idx) => [idx.stringified, idx]),
+    );
+    const expectedIndexesByName = new Map(
+      expectedIndexes.map((idx) => [idx.name, idx]),
+    );
+    const expectedIndexesByStructure = new Map(
+      expectedIndexes.map((idx) => [idx.stringified, idx]),
     );
 
-    const missingIndexes = expectedIndexes.filter(
-      ({ stringified }) => !existingIndexSet.has(stringified),
-    );
-    const extraIndexes = existingIndexes.filter(({ index, stringified }) => {
-      // Don't (try to) delete the default _id index
-      return (
-        !util.isDeepStrictEqual(index, [["_id", 1]]) &&
-        !expectedIndexSet.has(stringified)
+    // Compute which indexes need to be added.
+    const missingIndexes = expectedIndexes.filter((idx) => {
+      const matchingExistingStructure = existingIndexesByStructure.get(
+        idx.stringified,
+      );
+      const matchingExistingName = existingIndexesByName.get(idx.name);
+      return !(
+        matchingExistingStructure &&
+        matchingExistingName &&
+        matchingExistingStructure === matchingExistingName
       );
     });
 
-    for (const { index, options } of missingIndexes) {
-      Logger.info("Creating new index", {
-        model: model.name,
-        index,
-        options,
-      });
-      await collection.createIndex(index, Object.fromEntries(options));
-    }
-    for (const { name, index, options } of extraIndexes) {
+    // Compute which indexes need to be removed, and when
+    const [extraConflicting, extraNonConflicting] = existingIndexes.reduce(
+      (
+        acc: [ExistingIndexSpecification[], ExistingIndexSpecification[]],
+        idx: ExistingIndexSpecification,
+      ) => {
+        // Don't try to delete the default _id index.
+        if (util.isDeepStrictEqual(idx.index, [["_id", 1]])) return acc;
+        const matchingExpectedStructure = expectedIndexesByStructure.get(
+          idx.stringified,
+        );
+        const matchingExpectedName = expectedIndexesByName.get(idx.name);
+        // If an index matches both name and structure with an expected index,
+        // it is desired, and not extra, so we don't modify the accumulator.
+        if (
+          matchingExpectedName &&
+          matchingExpectedStructure &&
+          matchingExpectedName === matchingExpectedStructure
+        )
+          return acc;
+        if (matchingExpectedName || matchingExpectedStructure) {
+          // It is conflicting if it matches in name but not structure or
+          // structure but not name of some expected index.
+          acc[0].push(idx);
+        } else {
+          // It is nonconflicting if it matches neither.
+          acc[1].push(idx);
+        }
+
+        return acc;
+      },
+      [[], []],
+    );
+
+    const dropIndex = async (idx: ExistingIndexSpecification) => {
+      const { name, index, options } = idx;
       if (!name) {
         Logger.warn("Unable to drop index with no name", {
           model: model.name,
@@ -91,7 +131,7 @@ runIfLatestBuild(async () => {
           options,
           error: new Error("Unable to drop index with no name"),
         });
-        continue;
+        return;
       }
 
       Logger.info("Dropping unexpected index", {
@@ -100,6 +140,29 @@ runIfLatestBuild(async () => {
         options,
       });
       await collection.dropIndex(name);
+    };
+
+    // First, remove conflicting indexes.
+    for (const conflictingIndex of extraConflicting) {
+      await dropIndex(conflictingIndex);
+    }
+    // Then, add new indexes.
+    for (const { name, index, options } of missingIndexes) {
+      Logger.info("Creating new index", {
+        model: model.name,
+        index,
+        options,
+      });
+      // Prefer createIndexes to createIndex so we can specify an explicit name
+      // to match the one in our spec
+      await collection.createIndexes(
+        [{ key: Object.fromEntries(index), name }],
+        Object.fromEntries(options),
+      );
+    }
+    // Finally, drop non-conflicting indexes.
+    for (const extraIndex of extraNonConflicting) {
+      await dropIndex(extraIndex);
     }
   }
 });
