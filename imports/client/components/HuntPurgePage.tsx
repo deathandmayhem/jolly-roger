fa055b6a776900dd9ae76cc1401db90e2a76d5da
James <js@sugrono.com>
2026-01-09 08:29:34 +1100

    Add extra confirmation to purge button This makes the scary purge button much harder to trigger accidentally

--- imports/client/components/HuntPurgePage.tsx
+++ imports/client/components/HuntPurgePage.tsx
@@ -1,9 +1,12 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useState, useRef } from "react";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 import FormGroup from "react-bootstrap/FormGroup";
+import FormLabel from "react-bootstrap/FormGroup";
+import Hunts from "../../lib/models/Hunts";
 import { Navigate, useParams } from "react-router-dom";
 import isAdmin from "../../lib/isAdmin";
 import purgeHunt from "../../methods/purgeHunt";
@@ -14,6 +17,8 @@ import ModalForm from "./ModalForm";
 
 const HuntPurgePage = () => {
   const huntId = useParams<{ huntId: string }>().huntId;
+  const hunt = Hunts.findOne(huntId);
+  const [submitDisabled, setSubmitDisabled] = useState<boolean>(false);
 
   useBreadcrumb({
     title: "Purge Hunt",
@@ -47,14 +52,28 @@ const HuntPurgePage = () => {
         submitLabel="Purge"
         submitStyle="danger"
         onSubmit={doPurgeHuntContent}
+        submitDisabled={submitDisabled}
       >
         <p>Are you sure you want to purge all content from this Hunt?</p>
         <p>
           This will permanently delete all objects (puzzles, tags, messages,
-          etc.) associated with this Hunt, but keep the Hunt itselt, as well as
-          any users.
+          etc.) associated with this Hunt, but keep the Hunt itself, as well as
+          any users and their permissions.
+        </p>
+        <p>
+          This action will not delete any Google Drive content or resources
+          associated with this Hunt (e.g. in AWS S3).
         </p>
         <Alert variant="danger">This action cannot be undone.</Alert>
+        <FormLabel>
+          Type <code>{hunt?.name}</code> below to confirm
+        </FormLabel>
+        <FormControl
+          type="text"
+          onChange={(e) => {
+            setSubmitDisabled(e.currentTarget.value !== hunt?.name);
+          }}
+        />
       </ModalForm>
 
       <h1>Purge hunt</h1>
