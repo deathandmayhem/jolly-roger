8ac49a65e2e752e3d0961f09a0892e3ddc4882b7
James <js@sugrono.com>
2026-01-11 08:52:59 +1100

    Some tweaks to bookmarks updates
    - Made bookmarks a slightly less harsh yellow, in light mode
    - Used a styled div to add margin to the PuzzleList
    - Added logic to dynamically change the title of the "remaining" puzzle list on the main puzzle page to accurately reflect state

--- imports/client/components/PuzzleListPage.tsx
+++ imports/client/components/PuzzleListPage.tsx
@@ -697,6 +697,7 @@ const PuzzleListView = ({
       retainedDeletedPuzzles: PuzzleType[] | undefined,
       solvedOverConstrains: boolean,
       allPuzzlesCount: number,
+      listTitle: string,
     ) => {
       const maybeMatchWarning = solvedOverConstrains && (
         <Alert variant="info">
@@ -782,6 +783,7 @@ const PuzzleListView = ({
           break;
         }
       }
+
       return (
         <div>
           {maybeMatchWarning}
@@ -797,7 +799,7 @@ const PuzzleListView = ({
           </PuzzleListToolbar>
           {bookmarkedPuzzles.length > 0 && (
             <PuzzleGroupDiv>
-              <h4>Bookmarked</h4>
+              <h5>Bookmarked</h5>
               <RelatedPuzzleList
                 key="bookmarked"
                 relatedPuzzles={bookmarkedPuzzles}
@@ -810,7 +812,7 @@ const PuzzleListView = ({
                 subscribers={puzzleSubscribers}
                 puzzleUsers={puzzleUsers}
               />
-              <h4>All puzzles</h4>
+              <h5>{listTitle}</h5>
             </PuzzleGroupDiv>
           )}
           {listComponent}
@@ -845,10 +847,10 @@ const PuzzleListView = ({
       puzzleSubscribers,
       puzzleUsers,
       huntId,
-      searchString,
       showAddModalWithTags,
       canExpandAllGroups,
       expandAllGroups,
+      searchString,
     ],
   );
 
@@ -929,6 +931,16 @@ const PuzzleListView = ({
     );
   const retainedDeletedPuzzles =
     deletedPuzzles && puzzlesMatchingSearchString(deletedPuzzles);
+
+  const listTitle = useTracker(() => {
+    let listTitle = "All puzzles";
+    if (searchString.trim().length > 0) {
+      listTitle = "Search results";
+    } else if (!showSolved) {
+      listTitle = "Unsolved puzzles";
+    }
+    return listTitle;
+  }, [searchString, showSolved]);
 
   return (
     <div>
@@ -1063,6 +1075,7 @@ const PuzzleListView = ({
         retainedDeletedPuzzles,
         solvedOverConstrains,
         allPuzzles.length,
+        listTitle,
       )}
     </div>
   );
