f37bfed0c6df1b9c10673281a7a1c2eb205131e0
James <js@sugrono.com>
2026-01-12 19:54:48 +1100

    When we try to rename a tag, we should assign an existing one if it exists

--- imports/server/getOrCreateTagByName.ts
+++ imports/server/getOrCreateTagByName.ts
@@ -1,26 +1,37 @@
 import Logger from "../Logger";
 import Tags from "../lib/models/Tags";
+import { Meteor } from "meteor/meteor";
 
 export default async function getOrCreateTagByName(
   userId: string,
   huntId: string,
   name: string,
 ): Promise<string> {
-  const existingTag = await Tags.findOneAsync({ hunt: huntId, name });
+
+  const cleanName = name.trim().replace(/\s+/g, " ");
+
+  if (!cleanName) {
+    throw new Meteor.Error(400, "Tag name must not be empty");
+  }
+
+  const existingTag = await Tags.findOneAsync({
+    hunt: huntId,
+    name: { $regex: new RegExp(`^${escapeRegExp(cleanName)}$`, "i") }
+  });
   if (existingTag) {
     return existingTag._id;
   }
 
-  Logger.info("Creating a new tag", { hunt: huntId, name });
+  Logger.info("Creating a new tag", { hunt: huntId, name: cleanName });
   const newTagId = await Tags.insertAsync({
     hunt: huntId,
-    name,
+    name: cleanName,
     createdBy: userId,
   });
 
   // When creating a `group:*` tag, also ensure a matching `meta-for:` tag exists.
-  if (name.startsWith("group:")) {
-    const groupName = name.slice("group:".length);
+  if (cleanName.startsWith("group:")) {
+    const groupName = cleanName.slice("group:".length);
     const metaTagName = `meta-for:${groupName}`;
     await getOrCreateTagByName(userId, huntId, metaTagName);
   }
--- imports/server/methods/renameTag.ts
+++ imports/server/methods/renameTag.ts
@@ -1,5 +1,6 @@
 import { check } from "meteor/check";
 import Logger from "../../Logger";
+import Puzzles from "../../lib/models/Puzzles";
 import Tags from "../../lib/models/Tags";
 import renameTag from "../../methods/renameTag";
 import defineMethod from "./defineMethod";
@@ -18,7 +19,16 @@ defineMethod(renameTag, {
     check(this.userId, String);
 
     const tag = await Tags.findOneAsync(tagId);
-    if (tag) {
+
+    const existingTag = await Tags.findOneAsync({
+      hunt: tag?.hunt,
+      name,
+    });
+    if (existingTag) {
+      Puzzles.updateAsync({ tags: tagId }, { $set: { tags: existingTag._id } });
+      Puzzles.updateAsync({ tags: tagId }, { $pull: { tags: tagId } });
+      Tags.removeAsync(tagId);
+    } else if(tag) {
       Logger.info("Renaming tag", { tag: tagId, name });
       await Tags.updateAsync({ _id: tagId }, { $set: { name } });
     }
