acec92b272f95e0aa34a2fa74f5353b7e2669be9
James <js@sugrono.com>
2026-01-14 23:22:09 +1100

    Revert "Add special styling for puzzles that appear in multiple places"
    This reverts commit adc6847db29edc824707c65992e5f98b2538419b.

--- imports/client/components/PuzzleList.tsx
+++ imports/client/components/PuzzleList.tsx
@@ -1,46 +1,12 @@
-import React, { useContext } from "react";
-import styled, { css } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import type { ChatMessageType } from "../../lib/models/ChatMessages";
 import type { PuzzleType } from "../../lib/models/Puzzles";
 import type { TagType } from "../../lib/models/Tags";
 import Puzzle from "./Puzzle";
-import { PuzzleHoverContext } from "./PuzzleListPage";
 
 const StyledPuzzleListDiv = styled.div`
   margin-bottom: 1em;
-`;
-
-const PuzzleWrapper = styled.div<{ $isHovered: boolean; $isMulti: boolean }>`
-  transition:
-    background-color 0.1s ease-in-out,
-    box-shadow 0.1s ease-in-out,
-    transform 0.1s ease-in-out;
-  border-radius: 4px;
-  border: 1px solid transparent; /* Reserve space for border */
-
-  ${(props) =>
-    props.$isHovered &&
-    !props.$isMulti &&
-    css`
-      /* Standard Highlight for single items */
-      background-color: ${({ theme }) => theme.colors.autocompleteBackground};
-      box-shadow: 0 2px 4px ${({ theme }) => theme.colors.autocompleteShadow};
-      border-color: ${({ theme }) => theme.colors.autocompleteShadow};
-      position: relative;
-      z-index: 1;
-    `}
-
-  ${(props) =>
-    props.$isHovered &&
-    props.$isMulti &&
-    css`
-      /* "Linked" Highlight for items appearing multiple times */
-      background-color: ${({ theme }) => theme.colors.autocompleteBackground};
-      box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.info}, 0 2px 8px ${({ theme }) => theme.colors.info};
-      border-color: ${({ theme }) => theme.colors.info};
-      position: relative;
-      z-index: 2; /* Sit higher than single items */
-    `}
 `;
 
 const PuzzleList = React.memo(
@@ -56,8 +22,10 @@ const PuzzleList = React.memo(
     pinnedMessages,
     puzzleUsers,
   }: {
+    // The puzzles to show in this list
     puzzles: PuzzleType[];
     bookmarked: Set<string>;
+    // All tags for this hunt, including those not used by any puzzles
     allTags: TagType[];
     canUpdate: boolean;
     showSolvers: "viewers" | "hide" | "active";
@@ -67,45 +35,34 @@ const PuzzleList = React.memo(
     pinnedMessages?: ChatMessageType[] | null;
     puzzleUsers: Record<string, string[]>;
   }) => {
-    // Consume the expanded context
-    const { hoveredPuzzleId, setHoveredPuzzleId, multiOccurrenceIds } =
-      useContext(PuzzleHoverContext);
-
+    // This component just renders the puzzles provided, in order.
+    // Adjusting order based on tags, tag groups, etc. is to be done at
+    // a higher layer.
     return (
       <StyledPuzzleListDiv className="puzzle-list">
         {puzzles.map((puzzle) => {
           const puzzleId = puzzle._id;
-          const isHovered = hoveredPuzzleId === puzzleId;
-          const isMulti = multiOccurrenceIds.has(puzzleId);
-
           return (
-            <PuzzleWrapper
-              key={puzzleId}
-              $isHovered={isHovered}
-              $isMulti={isMulti}
-              onMouseEnter={() => setHoveredPuzzleId(puzzleId)}
-              onMouseLeave={() => setHoveredPuzzleId(null)}
-            >
-              <Puzzle
-                puzzle={puzzle}
-                bookmarked={bookmarked.has(puzzleId)}
-                allTags={allTags}
-                canUpdate={canUpdate}
-                suppressTags={suppressTags}
-                segmentAnswers={segmentAnswers}
-                showSolvers={showSolvers}
-                subscribers={
-                  subscribers && puzzleId in subscribers
-                    ? subscribers[puzzleId]
-                    : null
-                }
-                puzzleUsers={
-                  puzzleUsers && puzzleId in puzzleUsers
-                    ? puzzleUsers[puzzleId]
-                    : []
-                }
-              />
-            </PuzzleWrapper>
+            <Puzzle
+              key={puzzle._id}
+              puzzle={puzzle}
+              bookmarked={bookmarked.has(puzzle._id)}
+              allTags={allTags}
+              canUpdate={canUpdate}
+              suppressTags={suppressTags}
+              segmentAnswers={segmentAnswers}
+              showSolvers={showSolvers}
+              subscribers={
+                subscribers && puzzleId in subscribers
+                  ? subscribers[puzzleId]
+                  : null
+              }
+              puzzleUsers={
+                puzzleUsers && puzzleId in puzzleUsers
+                  ? puzzleUsers[puzzleId]
+                  : []
+              }
+            />
           );
         })}
       </StyledPuzzleListDiv>
