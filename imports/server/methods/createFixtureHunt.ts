563b54a4251e4f36c30805cc529b0380279430b7
Daniel Lepage <dplepage@gmail.com>
2026-02-16 22:21:00 -0500

    Add Hunts.methods.createGroupingDemo and corresponding fixture.

--- imports/server/methods/createFixtureHunt.ts
+++ imports/server/methods/createFixtureHunt.ts
@@ -1,6 +1,7 @@
 import { check } from "meteor/check";
 import { Meteor } from "meteor/meteor";
 import FixtureHunt from "../../FixtureHunt";
+import { FixtureHunt2 } from "../../FixtureHunt";
 import MeteorUsers from "../../lib/models/MeteorUsers";
 import { addUserToRole, userMayCreateHunt } from "../../lib/permission_stubs";
 import createFixtureHunt from "../../methods/createFixtureHunt";
@@ -23,5 +24,23 @@ defineMethod(createFixtureHunt, {
       $addToSet: { hunts: FixtureHunt._id },
     });
     await addUserToRole(this.userId, FixtureHunt._id, "operator");
+  },
+});
+
+defineMethod(new TypedMethod<void, void>("Hunts.methods.createGroupingDemo"), {
+  async run() {
+    check(this.userId, String);
+
+    if (!userMayCreateHunt(await MeteorUsers.findOneAsync(this.userId))) {
+      throw new Meteor.Error(401, "Must be allowed to create hunt");
+    }
+
+    await makeFixtureHunt(this.userId, FixtureHunt2);
+
+    // Make the user an operator
+    await MeteorUsers.updateAsync(this.userId, {
+      $addToSet: { hunts: FixtureHunt2._id },
+    });
+    await addUserToRole(this.userId, FixtureHunt2._id, "operator");
   },
 });
